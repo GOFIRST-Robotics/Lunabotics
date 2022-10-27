c2c3f4b0c7f2a5208d4a95c13d338bb81a76738d
Ubuntu <anthonybrog@gmail.com>
2022-10-26 19:56:37 -0500

    Added a subscriber for the currently requested state

--- src/rovr_control/rovr_control/main_control_node.py
+++ src/rovr_control/rovr_control/main_control_node.py
@@ -17,14 +17,47 @@ class RobotStates(StateMachine):
     teleop_to_dig = teleop.to(dig)
     teleop_to_halt = teleop.to(halt)
 
-def main():
-    print('Hi from the rovr_control node!')
+states = RobotStates()
 
-    print('State Machine test')
-    states = RobotStates()
-    print('Current State:', states.current_state)
-    states.current_state = states.auto
-    print('Current State:', states.current_state)
+class DS_Subscriber(Node):
+
+    def __init__(self):
+        super().__init__('ds_subscriber')
+        self.subscription = self.create_subscription(String, 'robot_command', self.listener_callback, 10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+        
+        # Switch our current state if a switch has been requested
+        if msg.data == states.teleop.value:
+            states.current_state = states.teleop
+        elif msg.data == states.auto.value:
+            states.current_state = states.auto
+        elif msg.data == states.dig.value:
+            states.current_state = states.dig
+        elif msg.data == states.halt.value:
+            states.current_state = states.halt
+
+        # Log the robot's current state
+        self.get_logger().info('Current State' % states.current_state)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    print('Hello from the rovr_control package!')
+    print('Initial State:', states.current_state)
+
+    ds_subscriber = DS_Subscriber()
+
+    rclpy.spin(ds_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    ds_subscriber.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
