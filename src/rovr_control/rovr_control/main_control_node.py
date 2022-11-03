6a5a88283d41ca1369992da9309191b31943065d
Ubuntu <anthonybrog@gmail.com>
2022-11-02 19:56:52 -0500

    Added all subscribers/publishers

--- src/rovr_control/rovr_control/main_control_node.py
+++ src/rovr_control/rovr_control/main_control_node.py
@@ -7,10 +7,9 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import PoseWithCovarianceStamped
-from geometry_msgs.msg import Vector3
 
 # Define our robot's initial state
-current_state = 'Emergency Stop'
+current_state = 'Auto Drive'
 
 # Define how fast we should drive forward while auto digging
 dig_speed = 1.0
@@ -24,23 +23,33 @@ class PublishersAndSubscribers(Node):
         self.actuators_publisher = self.create_publisher(String, 'cmd_actuators', 10)
         actuators_timer_period = 0.5  # how often to publish measured in seconds
         self.actuators_timer = self.create_timer(actuators_timer_period, self.actuators_timer_callback)
-
         # Velocity Publisher
         self.velocity_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         velocity_timer_period = 0.5  # how often to publish measured in seconds
         self.velocity_timer = self.create_timer(velocity_timer_period, self.velocity_timer_callback)
+        # Goal Publisher
+        self.goal_publisher = self.create_publisher(PoseStamped, 'Goal', 10)
+        goal_timer_period = 0.5  # how often to publish measured in seconds
+        self.goal_timer = self.create_timer(goal_timer_period, self.goal_timer_callback)
 
         # Robot Command Subscriber
         self.command_subscription = self.create_subscription(String, 'robot_command', self.command_callback, 10)
+        # EKF Subscriber
+        self.ekf_subscription = self.create_subscription(PoseWithCovarianceStamped, 'robot_pos_ekf/odom_combined', self.ekf_callback, 10)
+        # Velocity Subscriber
+        self.velocity_subscription = self.create_subscription(Twist, 'cmd_vel', self.velocity_callback, 10)
 
     def actuators_timer_callback(self):
         msg = String()
+
         if current_state == 'Auto Dig':
-             msg.data = 'DIGGER_ON'
+            msg.data = 'DIGGER_ON'
+            self.actuators_publisher.publish(msg)
+            self.get_logger().info('Publishing: "%s"' % msg.data)
         elif current_state == 'Emergency Stop':
             msg.data = 'STOP_ALL_ACTUATORS'
-        self.actuators_publisher.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+            self.actuators_publisher.publish(msg)
+            self.get_logger().info('Publishing: "%s"' % msg.data)
 
     def velocity_timer_callback(self):
         msg = Twist()
@@ -66,6 +75,24 @@ class PublishersAndSubscribers(Node):
         self.get_logger().info(f'Publishing Angular Speed: {msg.angular.x}, Linear Speed: {msg.linear.x}')
 
 
+    def goal_timer_callback(self):
+
+        # We only need to publish a goal if we are currently autonomous
+        if current_state == 'Auto Drive':
+            msg = PoseStamped()
+
+            msg.pose.position.x = 0.0 # TODO: How do we calculate this???
+            msg.pose.position.y = 0.0 # TODO: How do we calculate this???
+            msg.pose.position.z = 0.0 # TODO: How do we calculate this???
+
+            msg.pose.orientation.x = 0.0 # TODO: How do we calculate this???
+            msg.pose.orientation.y = 0.0 # TODO: How do we calculate this???
+            msg.pose.orientation.z = 0.0 # TODO: How do we calculate this???
+        
+            self.goal_publisher.publish(msg)
+            self.get_logger().info(f'Publishing Position: {msg.pose.position}, Orientation: {msg.pose.orientation}')
+
+
     def command_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.data)
         
@@ -82,7 +109,28 @@ class PublishersAndSubscribers(Node):
         # Log the robot's current state
         self.get_logger().info('Current State: "%s"' % current_state)
 
+    
+    def ekf_callback(self, msg):
+        pos_x = msg.pose.pose.position.x
+        pos_y = msg.pose.pose.position.y
+        pos_z = msg.pose.pose.position.z
 
+        orientation_x = msg.pose.pose.orientation.x
+        orientation_y = msg.pose.pose.orientation.y
+        orientation_z = msg.pose.pose.orientation.z
+
+        covariance = msg.pose.covariance
+
+        self.get_logger().info(f'Received a message with covariance of: {covariance}')
+
+
+    def velocity_callback(self, msg):
+        angular_velocity = msg.angular
+        linear_velocity = msg.linear
+
+        self.get_logger().info(f'Received a velocity message with Angular: {angular_velocity} and Linear: {linear_velocity}')
+        
+        
 def main(args=None):
     rclpy.init(args=args)
 
