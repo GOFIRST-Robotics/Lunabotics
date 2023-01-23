331d6fcb45ebe9fd9cb3b3cd4f0487c8bb478857
Anthony Brogni <anthonybrog@gmail.com>
2023-01-22 19:27:16 -0600

    placeholder for now

--- src/motor_control/src/motor_control_node.cpp
+++ src/motor_control/src/motor_control_node.cpp
@@ -112,7 +112,7 @@ private:
   void CAN_callback(const can_msgs::msg::Frame::SharedPtr can_msg) const
   {
     U32 id = can_msg->id;
-    S32[] data = can_msg->data; // TODO: Figure out how to decode this data into something useful
+    auto data = can_msg->data; // TODO: Figure out how to decode this data into something useful
   }
   void actuators_callback(const std_msgs::msg::String::SharedPtr msg) const
   {
