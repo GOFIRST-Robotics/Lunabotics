24e6cf00d98e22a6b9fe8b84c170c6f0cc48b3d7
Anthony Brogni <anthonybrog@gmail.com>
2023-01-22 19:46:15 -0600

    Added decoding of status frame 1

--- src/motor_control/src/motor_control_node.cpp
+++ src/motor_control/src/motor_control_node.cpp
@@ -19,8 +19,9 @@
 #include <string>
 #include <stdint.h>
 
-typedef int32_t S32; // Unsigned 32-bit integer
-typedef uint32_t U32; // Signed 32-bit integer
+typedef int32_t S32; // Signed 32-bit integer
+typedef uint32_t U32; // Unsigned 32-bit integer
+typedef uint8_t U8; // Unsigned 8-bit integer
 
 // Global Variables
 void send_can(U32 id, S32 data);
@@ -112,7 +113,13 @@ private:
   void CAN_callback(const can_msgs::msg::Frame::SharedPtr can_msg) const
   {
     U32 id = can_msg->id;
-    auto data = can_msg->data; // TODO: Figure out how to decode this data into something useful
+    U8[8] data = can_msg->data; // bytes 0-3 = eRPM, bytes 4-5 = average current, bytes 6-7 = latest duty cycle
+
+    double eRPM = (double)(data[0]<<24 + data[1]<<16 + data[2]<<8 + data[3]);
+    double avgMotorCurrent = (double)((data[4]<<8 + data[5]) / 10);
+    double dutyCycleNow = (double)((data[6]<<8 + data[7]) / 1000);
+
+    RCLCPP_INFO(this->get_logger(), "Recieved status frame from CAN ID: %i with an eRPM of: %f", id, eRPM);
   }
   void actuators_callback(const std_msgs::msg::String::SharedPtr msg) const
   {
