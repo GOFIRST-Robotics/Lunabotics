95a4c8b15586cc9c3f9b4c261665957d0bd65f6b
Anthony Brogni <anthonybrog@gmail.com>
2023-02-25 20:09:35 -0600

    Remove currentState variable
    I think this is actually redundant and our code will be better without it.

--- Arduino/linear_actuator/linear_actuator.ino
+++ Arduino/linear_actuator/linear_actuator.ino
@@ -4,8 +4,6 @@ const int retract_pin = 9; // motor driver pin
 const int limit_switch_extend = 8; // limit switch pin
 const int limit_switch_retract = 7; // limit switch pin
 
-int currentState = 0; // 0 will be retracted, 1 will be extended
-
 // put setup code here to run once:
 void setup() {
   Serial.begin(9600); // open serial communication with a baud rate of 9600 bps
@@ -20,18 +18,16 @@ void setup() {
 
 // put main code here to run repeatedly:
 void loop() {
-  if (Serial.available()) { // check if serial communication is available
+  if (Serial.available()) { // check if data is available to be read
     char data_received = Serial.read(); // read one byte from the serial buffer and save to data_received
 
-    if (data_received == '1' and currentState == 0) extend(); // extend the linear actuator
-    if (data_received == '0' and currentState == 1) retract(); // retract the linear actuator
+    if (data_received == '1') extend(); // extend the linear actuator
+    if (data_received == '0') retract(); // retract the linear actuator
   }
 }
 
 // extend the linear actuator, using a limit switch for feedback
 void extend() {
-  currentState = 1;
-
   while (digitalRead(limit_switch_extend) == HIGH) { // loop until the limit switch is pressed
     digitalWrite(extend_pin, HIGH);
     digitalWrite(retract_pin, LOW);
@@ -43,8 +39,6 @@ void extend() {
 
 // retract the linear actuator, using a limit switch for feedback
 void retract() {
-  currentState = 0;
-
   while (digitalRead(limit_switch_retract) == HIGH) { // loop until the limit switch is pressed
     digitalWrite(extend_pin, LOW);
     digitalWrite(retract_pin, HIGH);
