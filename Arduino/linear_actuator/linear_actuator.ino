e0668cea850e6f19a936177a8bdcc1cdca68334c
Anthony Brogni <anthonybrog@gmail.com>
2023-02-27 18:02:09 -0600

    Blink the built-in LED

--- Arduino/linear_actuator/linear_actuator.ino
+++ Arduino/linear_actuator/linear_actuator.ino
@@ -12,8 +12,10 @@ void setup() {
   pinMode(retract_pin, OUTPUT);
   pinMode(limit_switch_extend, INPUT);
   pinMode(limit_switch_retract, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT); // The built-in LED
 
   stop_actuator(); // stop the linear actuator
+  digitalWrite(LED_BUILTIN, LOW); // Turn off the built-in LED
 }
 
 // put main code here to run repeatedly:
@@ -28,6 +30,8 @@ void loop() {
 
 // extend the linear actuator, using a limit switch for feedback
 void extend() {
+  digitalWrite(LED_BUILTIN, HIGH); // Turn on the built-in LED
+
   while (digitalRead(limit_switch_extend) == HIGH) { // loop until the limit switch is pressed
     digitalWrite(extend_pin, HIGH);
     digitalWrite(retract_pin, LOW);
@@ -35,10 +39,13 @@ void extend() {
 
   stop_actuator(); // stop the linear actuator
   Serial.write('3'); // send a confirmation message back to the Jetson
+  digitalWrite(LED_BUILTIN, LOW); // Turn off the built-in LED
 }
 
 // retract the linear actuator, using a limit switch for feedback
 void retract() {
+  digitalWrite(LED_BUILTIN, HIGH); // Turn on the built-in LED
+  
   while (digitalRead(limit_switch_retract) == HIGH) { // loop until the limit switch is pressed
     digitalWrite(extend_pin, LOW);
     digitalWrite(retract_pin, HIGH);
@@ -46,6 +53,7 @@ void retract() {
 
   stop_actuator(); // stop the linear actuator
   Serial.write('4'); // send a confirmation message back to the Jetson
+  digitalWrite(LED_BUILTIN, LOW); // Turn off the built-in LED
 }
 
 // stop the linear actuator
