329a03ac8ce5ae99d6c05377db5cc987fdd653f3
Anthony Brogni <anthonybrog@gmail.com>
2023-03-20 20:28:17 -0500

    Revert "Simplify the Arduino code"
    This reverts commit 1d48598dcee5e52c1b4acae542d01bfbfda74bed.

--- Arduino/linear_actuator/linear_actuator.ino
+++ Arduino/linear_actuator/linear_actuator.ino
@@ -1,71 +1,94 @@
-#include <ezButton.h> // This library makes it easier for us to work with limit switches
+#include <ezButton.h>
 
-const int extend_pin = 10; // motor driver forwards pin
-const int retract_pin = 9; // motor driver reverse pin
+const int extend_pin = 10; // motor driver pin
+const int retract_pin = 9; // motor driver pin
 
-ezButton limit_switch_extend(8); // extend limit switch pin
-ezButton limit_switch_retract(7); // retract limit switch pin
+ezButton limit_switch_extend(8); // limit switch pin
+ezButton limit_switch_retract(7); // limit switch pin
 
-// These booleans help keep track of the current state of the linear actuator
-bool extending = false;
-bool retracting = false;
-
-// put setup code in this method to run once:
+// put setup code here to run once:
 void setup() {
   Serial.begin(9600); // open serial communication with a baud rate of 9600 bps
 
-  pinMode(extend_pin, OUTPUT); // Declare extend_pin to be an OUTPUT pin
-  pinMode(retract_pin, OUTPUT); // Declare retract_pin to be an OUTPUT pin
-  pinMode(LED_BUILTIN, OUTPUT); // This is the built-in LED on the Arduino
+  pinMode(extend_pin, OUTPUT);
+  pinMode(retract_pin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT); // The built-in LED
 
   stop_actuator(); // stop the linear actuator
+  digitalWrite(LED_BUILTIN, LOW); // Turn off the built-in LED
 }
 
-// put main loop code in this method to run repeatedly:
+// put main code here to run repeatedly:
 void loop() {
-  limit_switch_extend.loop(); // this method from the ezButton library needs to be called every code cycle
-  limit_switch_retract.loop(); // this method from the ezButton library needs to be called every code cycle
-
-  if (Serial.available()) { // check if data is available to be read from the serial buffer
+  if (Serial.available()) { // check if data is available to be read
     char data_received = Serial.read(); // read one byte from the serial buffer and save to data_received
     if (data_received == 'e') extend(); // extend the linear actuator
     if (data_received == 'r') retract(); // retract the linear actuator
   }
-
-  // check if the extend limit switch is pressed and we are currently extending
-  if(limit_switch_extend.isPressed() && extending) {
-    stop_actuator(); // stop the linear actuator
-    Serial.write('f'); // send a message over serial to the Jetson
-  }
-
-  // check if the retract limit switch is pressed and we are currently retracting
-  if(limit_switch_retract.isPressed() && retracting) {
-    stop_actuator(); // stop the linear actuator
-    Serial.write('s'); // send a message over serial to the Jetson
-  }
 }
 
-// extend the linear actuator by running the motor forwards
+// extend the linear actuator, using a limit switch for feedback
 void extend() {
-  extending = true;
+  digitalWrite(LED_BUILTIN, HIGH); // Turn on the built-in LED
   digitalWrite(extend_pin, HIGH);
   digitalWrite(retract_pin, LOW);
-  digitalWrite(LED_BUILTIN, HIGH); // Turn on the built-in LED
+
+  while (true) { // loop until the limit switch is pressed
+    limit_switch_extend.loop();
+    if(limit_switch_extend.isPressed()) {
+      break;
+    }
+
+    if (Serial.available()) { // check if data is available to be read
+      char data_received = Serial.read(); // read one byte from the serial buffer and save to data_received
+      if (data_received == 'e') {
+        extend(); // extend the linear actuator
+        break;
+      }
+      if (data_received == 'r') {
+        retract(); // retract the linear actuator
+        break;
+      }
+    }
+  }
+
+  stop_actuator(); // stop the linear actuator
+  Serial.write('f'); // send a confirmation message back to the Jetson
+  digitalWrite(LED_BUILTIN, LOW); // Turn off the built-in LED
 }
 
-// retract the linear actuator by running the motor in reverse
+// retract the linear actuator, using a limit switch for feedback
 void retract() {
-  retracting = true;
+  digitalWrite(LED_BUILTIN, HIGH); // Turn on the built-in LED
   digitalWrite(extend_pin, LOW);
   digitalWrite(retract_pin, HIGH);
-  digitalWrite(LED_BUILTIN, HIGH); // Turn on the built-in LED
+  
+  while (true) { // loop until the limit switch is pressed
+    limit_switch_retract.loop();
+    if(limit_switch_retract.isPressed()) {
+      break;
+    }
+
+    if (Serial.available()) { // check if data is available to be read
+      char data_received = Serial.read(); // read one byte from the serial buffer and save to data_received
+      if (data_received == 'e') {
+        extend(); // extend the linear actuator
+        break;
+      }
+      if (data_received == 'r') {
+        retract(); // retract the linear actuator
+        break;
+      }
+    }
+  }
+
+  stop_actuator(); // stop the linear actuator
+  Serial.write('s'); // send a confirmation message back to the Jetson
+  digitalWrite(LED_BUILTIN, LOW); // Turn off the built-in LED
 }
 
-// stop the linear actuator motor
+// stop the linear actuator
 void stop_actuator() {
   digitalWrite(extend_pin, LOW);
   digitalWrite(retract_pin, LOW);
-  digitalWrite(LED_BUILTIN, LOW); // Turn off the built-in LED
-  extending = false;
-  retracting = false;
 }
