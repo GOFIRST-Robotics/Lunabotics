0317a050dfdce99e3f9401536731fd188b59ad50
Benjamin Chen <chen5713@umn.edu>
2023-04-11 21:47:48 -0500

    create scripts to start realsense, transform, and rtabmap nodes

---
+++ scripts/realsense_start.sh
@@ -0,0 +1,1 @@
+ros2 launch realsense2_camera rs_launch.py depth_module.profile:=848x480x6 rgb_camera.profile:=848x480x6 pointcloud.enable:=true
---
+++ scripts/transform_publisher_start.sh
@@ -0,0 +1,1 @@
+ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link camera_color_optical_frame
