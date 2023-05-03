17d17721d6e1e74b2a94e83e6079d656b3a9d9a1
Isopod00 <anthonybrog@gmail.com>
2023-05-02 21:28:41 -0500

    moved this new launch file to a better location

---
+++ src/motor_control/launch/realsense_launch.py
@@ -0,0 +1,29 @@
+import launch
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+def generate_launch_description():
+
+    realsense_camera_node = ComposableNode(
+        package='realsense2_camera',
+        plugin='realsense2_camera::RealSenseNodeFactory',
+        name='realsense2_camera',
+        namespace='',
+        parameters=[{
+            'depth_module.profile': '1280x720x30',
+            'rgb_camera.profile': '1920x1080x30',
+        }],
+        remappings=[('/color/image_raw', '/image'),
+                    ('/color/camera_info', '/camera_info')]
+    )
+
+    apriltag_container = ComposableNodeContainer(
+        package='rclcpp_components',
+        name='apriltag_container',
+        namespace='',
+        executable='component_container_mt',
+        composable_node_descriptions=[realsense_camera_node],
+        output='screen'
+    )
+
+    return launch.LaunchDescription([apriltag_container])
