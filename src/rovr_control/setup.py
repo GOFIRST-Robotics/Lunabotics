83366c7f2530f222cae8d1416ad92d39e0be260d
Isopod00 <anthonybrog@gmail.com>
2023-05-03 14:20:23 -0500

    Move realsense launch file

--- src/rovr_control/setup.py
+++ src/rovr_control/setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
