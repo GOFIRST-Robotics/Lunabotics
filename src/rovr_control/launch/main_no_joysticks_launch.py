6d39086e28a2e15b78447906ca32a2b5007544b1
Victor Hofstetter <victorbarroshof@gmail.com>
2023-05-24 10:47:22 -0500

    implement automatic target ip setting for camera

--- src/rovr_control/launch/main_no_joysticks_launch.py
+++ src/rovr_control/launch/main_no_joysticks_launch.py
@@ -1,21 +1,35 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import subprocess
+import re
+
 
 def generate_launch_description():
-    ld = LaunchDescription()
+  def get_target_ip(target: str, default: str | None):
+    try:
+      nmap = subprocess.Popen(
+          ('nmap', '-sn', '192.168.1.1/24'), stdout=subprocess.PIPE)
+      grep = subprocess.check_output(('grep', target), stdin=nmap.stdout)
+      nmap.wait()
+      res = grep.decode()
+      return re.sub(r'.*\((.*)\).*', r'\g<1>', res) or default
+    except:
+      return default
 
-    rovr_control = Node(
-        package="rovr_control",
-        executable="main_control_node",
-        parameters=[{'ip': '192.168.1.117'}],
-    )
+  ld = LaunchDescription()
 
-    motor_control = Node(
-        package="motor_control",
-        executable="motor_control_node"
-    )
+  rovr_control = Node(
+      package="rovr_control",
+      executable="main_control_node",
+      parameters=[{'ip': get_target_ip('blixt-G14', '192.168.1.117')}],
+  )
 
-    ld.add_action(rovr_control)
-    ld.add_action(motor_control)
+  motor_control = Node(
+      package="motor_control",
+      executable="motor_control_node"
+  )
 
-    return ld
+  ld.add_action(rovr_control)
+  ld.add_action(motor_control)
+
+  return ld
