e587c60999aa2b34ce8c201010903b1323a6dc94
umnrobotics <robotics@umn.edu>
2023-10-03 18:21:45 -0500

    finished prototype functionality Fixed typos and added to setup.py

--- src/conveyor/conveyor/check_load.py
+++ src/conveyor/conveyor/check_load.py
@@ -6,8 +6,8 @@ import pyrealsense2 as rs
 
 YRESOLUTION = 480 # whatever y componenet (vertical) resolution the camera is
 XRESOLUTION = 640 # same as above, but in the x direction
-DISTANCETHRESH = .25 # how small should the distance between top and conveyor be before offload?
-POLLRATE = 1 # Wait time between each distance check
+DISTANCETHRESH = .25 # how small should the distance between top and conveyor be before offload (in meters)?
+POLLRATE = 1 # Wait time between each distance check (in seconds)
 CONSECUTIVECYCLES = 4 # Make sure the reading is consistent
 
 class check_load(Node):
@@ -17,16 +17,16 @@ class check_load(Node):
         self.pub = self.create_publisher(Bool, 'readyDump', 10)
         self.prior_checks = []
         self.timer = self.create_timer(POLLRATE, self.publish_distance)
-        pipeline = rs.pipeline()
+        self.pipeline = rs.pipeline()
 
         config = rs.config()
         config.enable_stream(rs.stream.depth, XRESOLUTION, YRESOLUTION, rs.format.z16, 30)
-        pipeline.start(config)
+        self.pipeline.start(config)
 
     def publish_distance(self):
         try:
             frames = self.pipeline.wait_for_frames()
-            depth = frames.get_depth_frane()
+            depth = frames.get_depth_frame() #not .get_depth_frane()
             average_tally = 0
             for y in range(YRESOLUTION):
                 for x in range(XRESOLUTION):
@@ -41,11 +41,30 @@ class check_load(Node):
 
             if False not in self.prior_checks and len(self.prior_checks) >= CONSECUTIVECYCLES:
                 msg.data = True
+                print("True")
             else:
                 msg.data = False
+                print("False")
 
             self.pub.publish(msg)
 
         except Exception as e:
             print(e)
             pass
+
+
+def main(args=None):
+    """The main function."""
+    rclpy.init(args=args)
+    print("Initializing the Conveyor subsystem!")
+
+    node = check_load()
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+# This code does NOT run if this file is imported as a module
+if __name__ == "__main__":
+    main()
