9e13ae773623677178406835ac39360fdbe49c08
Jonathan Blixt <jmblixt3@gmail.com>
2023-10-18 19:25:42 -0500

    tested dynamic cropping. It worked.

--- src/conveyor/conveyor/ros_check_load.py
+++ src/conveyor/conveyor/ros_check_load.py
@@ -1,32 +1,35 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Bool
-from sensor_msgs.msg import Image, float32
+from std_msgs.msg import Bool, Float32
+from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
 import math
 
-CONVEYORSIZEX = 0.5  # width of the conveyor in meters
-CONVEYORSIZEY = 0.5  # length of the conveyor in meters
+CONVEYORSIZEY = 0.7112  # width of the conveyor in meters
+CONVEYORSIZEX = 0.623  # length of the conveyor in meters
 CONVEYORTOCAM = 0.3  # distance from camera to top of conveyor in meters.
 DISTANCETHRESH = 200  # how small should the distance between top and conveyor be before offload (in meters)?
-POLLRATE = 0.5  # Wait time between each distance check (in seconds)
+POLLRATE = 0.2  # Wait time between each distance check (in seconds)
 CONSECUTIVECYCLES = 4  # Make sure the reading is consistent
 conveyor_height_topic = "/conveyor/height"  # should be meters, if not, convert
 
 
 class ros_check_load(Node):
     def __init__(self):
+        self.height = .5
         super().__init__("check_load")
         self.bridge = CvBridge()
         self.pub = self.create_publisher(Bool, "readyDump", 10)
+        self.imagePub = self.create_publisher(Image, "imageResize", 10)
         self.prior_checks = []
-        depth_image_topic = "/camera/depth/image_rect_raw"  # Switch to /depth/image_rect_raw when running on robot
-        self.getConveyorHeight = self.create_subscription(float32, conveyor_height_topic, self.setHeight, 10)
+        depth_image_topic = "/camera/camera/depth/image_rect_raw"  # Switch to /depth/image_rect_raw when running on robot
+        self.getConveyorHeight = self.create_subscription(Float32, conveyor_height_topic, self.setHeight, 10)
         self.oneTimeSub = self.create_subscription(Image, depth_image_topic, self.setParamCallback, 1)
         self.subscriber = self.create_subscription(Image, depth_image_topic, self.depth_image_callback, 10)
         self.timer = self.create_timer(POLLRATE, self.publish_distance)
         self.depth_image = None
+        
 
     def setHeight(self, msg):
         self.conveyor_height = msg.data
@@ -34,11 +37,6 @@ class ros_check_load(Node):
     def setParamCallback(self, msg):
         self.img_height = msg.height
         self.img_width = msg.width
-        # center_x, center_y = width // 2, height // 2
-        # self.start_x = max(0, center_x - width // 4)
-        # self.end_x = min(width, center_x + width // 4)
-        # self.start_y = max(0, center_y - height // 4)
-        # self.end_y = min(height, center_y + (height // 4))
         self.destroy_subscription(self.oneTimeSub)
 
     def depth_image_callback(self, msg):
@@ -50,26 +48,29 @@ class ros_check_load(Node):
 
             # find the degrees of vision occupied by the conveyor belt
             perceptionChangeX = (
-                2 * math.atan((0.5 * CONVEYORSIZEX) / (self.conveyor_height + CONVEYORTOCAM)) * (180 / math.PI)
+                (2 * (math.atan((0.5 * CONVEYORSIZEX) / (self.conveyor_height + CONVEYORTOCAM)))) * (180 / math.pi)
             )
             perceptionChangeY = (
-                2 * math.atan((0.5 * CONVEYORSIZEY) / (self.conveyor_height + CONVEYORTOCAM)) * (180 / math.PI)
+                (2 * (math.atan((0.5 * CONVEYORSIZEY) / (self.conveyor_height + CONVEYORTOCAM)))) * (180 / math.pi)
             )
 
             percentFOVx = (
-                perceptionChangeX / 85.2
+                perceptionChangeX / 86
             )  # compare the degrees of vision it would occupy to the FOV of the realsense
-            percentFOVy = perceptionChangeY / 58.5
+            percentFOVy = perceptionChangeY / 57
 
             change_x = (
-                self.img_width * percentFOVx
-            ) / 2  # get the number of pixels that it's occupying. divide by 2 so it's a +- situation
-            change_y = (self.img_height * percentFOVy) / 2
+                int(self.img_width * percentFOVx / 2)
+            )   # get the number of pixels that it's occupying. divide by 2 so it's a +- situation
+            change_y = int((self.img_height * percentFOVy) / 2)
 
             center_x, center_y = self.img_width // 2, self.img_height // 2
-            resized_img = depth[center_y - change_y : center_y + change_y, center_x - change_x : center_x + change_x]
+            denoised_image = cv2.GaussianBlur(depth, (5, 5), 0)
+            resized_img = denoised_image[center_y - change_y : center_y + change_y, center_x - change_x : center_x + change_x]
 
-            denoised_image = cv2.GaussianBlur(resized_img, (5, 5), 0)
+            
+
+            self.imagePub.publish(self.bridge.cv2_to_imgmsg(resized_img, "passthrough"))
 
             print(denoised_image.mean())
 
