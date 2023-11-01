b774ff95d5db23a97f5b8db467dd90da9e3267e8
Jonathan <jmblixt3@gmail.com>
2023-11-01 18:23:48 -0500

    Isaac ros add zed slam support (#87)
    * Start work on new nav2
    
    * Squashed commit of the following:
    
    commit 3c25a93f916d07dfea154823442fafe10135c8e1
    Author: Alex Berg <56053786+BergerKing333@users.noreply.github.com>
    Date:   Tue Oct 31 18:10:40 2023 -0500
    
        Realsense wrapper test (#84)
    
        * Add realsense wrapper
    
        * starting experiment with ros wrapper
    
        * Fixing the subscriber setup and reading methods
    
        * Adds image resizing
    
        * working ros realsense
    
        * Added denoising code for testing
    
        * Fix realsense wrapper launch error
    
        * Got image denoising working.
    
        * first try at dynamic cropping
    
        * Renamed conveyor_height_topic to "/conveyor/height"
    
        * Autoformatting
    
        Also rosdep does not have a definition for "cv_bridge_boost" so I removed that from package.xml
    
        * tested dynamic cropping. It worked.
    
        * prevent camera from trying to display >100% pixels
    
        * Launch file topic remappings
    
        * comments and todo stuff.
    
        * Camera resolution change in launch file
    
        * adding source dependency
    
        * fixed launch file
    
        * things worked and then stopped working
    
        * Fixed it again, so it works through remapping
    
        * fixing the topic name
    
        * Removed old
    
        * Destroy node if camera does not work
    
        * adding error handling
    
        * better error handling, plus some documentation
    
        * swap try except to if. relasense still runs though
    
        ---------
    
        Co-authored-by: umnrobotics <robotics@umn.edu>
        Co-authored-by: NathanStangler <nathanstangler1@gmail.com>
        Co-authored-by: Anthony Brogni <anthonybrog@gmail.com>
        Co-authored-by: Jonathan Blixt <jmblixt3@gmail.com>
    
    commit bff7627770f9a495f4a66cd3ccf036b501cedcc0
    Author: Jonathan <jmblixt3@gmail.com>
    Date:   Tue Oct 31 16:41:17 2023 -0500
    
        Update README.md
    
        This is to add a description of having a layer specific to umn robotics
    
    * Add recorded data for zed
    
    * Have initial setup w/o nav2
    
    * Create isaac_ros launch package and move packages
    
    * Fix config
    
    ---------
    
    Co-authored-by: Anthony Brogni <anthonybrog@gmail.com>
    Co-authored-by: umnrobotics <robotics@umn.edu>

--- src/rovr_control/setup.py
+++ src/rovr_control/setup.py
@@ -11,7 +11,10 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join("share", package_name), glob("launch/*launch.[pxy][yma]*")),
+        (
+            os.path.join("share", package_name),
+            glob("launch/*launch.[pxy][yma]*", recursive=True),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
