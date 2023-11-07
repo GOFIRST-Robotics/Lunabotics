8032698b956224694eba4cde671d6d9b7d0ab648
akshatarinav <akshatarinav@gmail.com>
2023-11-07 17:51:49 -0600

    Modified joystick inputs

--- src/rovr_control/rovr_control/main_control_node.py
+++ src/rovr_control/rovr_control/main_control_node.py
@@ -171,10 +171,11 @@ class MainControlNode(Node):
         # PUT TELEOP CONTROLS BELOW #
 
         if self.state == states["Teleop"]:
-            # Drive the robot using joystick input during Teleop # TODO: Fix the below code for swerve drive
-            drive_power = msg.axes[RIGHT_JOYSTICK_VERTICAL_AXIS] * self.max_drive_power  # Forward power
+            # Drive the robot using joystick input during Teleop 
+            forward_power = msg.axes[RIGHT_JOYSTICK_VERTICAL_AXIS] * self.max_drive_power  # Forward power
+            horizontal_power = msg.axes[RIGHT_JOYSTICK_HORIZONTAL_AXIS] * self.max_drive_power #Horizontal power
             turn_power = msg.axes[LEFT_JOYSTICK_HORIZONTAL_AXIS] * self.max_turn_power  # Turning power
-            self.drive_power_publisher.publish(Twist(linear=Vector3(x=drive_power), angular=Vector3(z=turn_power)))
+            self.drive_power_publisher.publish(Twist(linear=Vector3(x=horizontal_power, y=forward_power),angular=Vector3(z=turn_power)))
 
             # Check if the conveyor button is pressed #
             if msg.buttons[X_BUTTON] == 1 and buttons[X_BUTTON] == 0:
