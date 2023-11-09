012a4beff44e113fbfad20535032f1c3c12c4d03
alex berg <aaberg333@gmail.com>
2023-11-09 17:11:56 -0600

    added code to filter through multiple tags

--- src/apriltag/apriltag/apriltag.py
+++ src/apriltag/apriltag/apriltag.py
@@ -28,16 +28,27 @@ class Apriltag(Node):
     def sendTransform(self, msg):
         if len(msg.detections) == 0:
             return
+        
+        """Should allow tag filter by ID. skipping bc i only have 1 tag rn"""
+        # for tag in msg.detections:
+        #     print(tag.id)
+        #     if tag.id == 3:
+        #         home = tag
+        #     elif tag.id == 4:
+        #         berm = tag
+        
+        home = msg.detections[0]    # comment this out if you want to use the loop
+
         t = TransformStamped()
         t.child_frame_id = "odom"
         t.header.frame_id = 'map'
         t.header.stamp = self.get_clock().now().to_msg()
-        t.transform.translation.x = msg.detections[0].pose.pose.pose.position.x
-        t.transform.translation.y = msg.detections[0].pose.pose.pose.position.y
-        # t.transform.translation.z = msg.detections[0].pose.pose.pose.position.z
-        t.transform.rotation.x = msg.detections[0].pose.pose.pose.orientation.x
-        t.transform.rotation.y = msg.detections[0].pose.pose.pose.orientation.y
-        t.transform.rotation.z = msg.detections[0].pose.pose.pose.orientation.z
+        t.transform.translation.x = home.pose.pose.pose.position.x
+        t.transform.translation.y = home.pose.pose.pose.position.y
+        # t.transform.translation.z = home.pose.pose.pose.position.z
+        t.transform.rotation.x = home.pose.pose.pose.orientation.x
+        t.transform.rotation.y = home.pose.pose.pose.orientation.y
+        t.transform.rotation.z = home.pose.pose.pose.orientation.z
         self.tf_broadcaster.sendTransform(t)
         
 def main(args=None):
