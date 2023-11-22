03b6a29ead9bac276a57c5c02a36f69f50fbd38a
alex berg <aaberg333@gmail.com>
2023-11-21 18:44:13 -0600

    parsing the xml file in python.

--- src/apriltag/apriltag/apriltag.py
+++ src/apriltag/apriltag/apriltag.py
@@ -4,6 +4,8 @@ import rclpy
 from rclpy.node import Node
 from tf2_ros import TransformBroadcaster
 from isaac_ros_apriltag_interfaces.msg import AprilTagDetectionArray
+import xml.etree.ElementTree as ET
+import os
 
 """Make sure to turn on the camera using 
 ros2 launch isaac_ros_apriltag isaac_ros_apriltag_usb_cam.launch.py
@@ -12,11 +14,19 @@ or nothing here will work"""
 class Apriltag(Node):
     def __init__(self):
         super().__init__('apriltag')
+        current_dir = os.getcwd()
+
+        # swap comment based on the field.
+        relative_path = "src/apriltag/apriltag/apriltag_location_nasa.urdf.xarco"
+        # relative_path = "src/apriltag/apriltag/apriltag_location_ucf.urdf.xarco"
+        
+        self.file_path = os.path.join(current_dir, relative_path)
         self.transforms = self.create_subscription(AprilTagDetectionArray, "/tag_detections", self.sendTransform, 10)
         # self.transforms = self.create_subscription(AprilTagDetectionArray, "/tag_detections", self.sendTransform, 10)
         self.tf_broadcaster = TransformBroadcaster(self)
         # self.timer = self.create_timer(0.1, self.test)
         
+    """Delete this after stuff actually works."""
     def test(self):
         t = TransformStamped()
         t.child_frame_id = "odom"
@@ -40,31 +50,36 @@ class Apriltag(Node):
             return
         
         # tag filtering. For now, the tag at (0,0) is id 3, the berm is id 1
-        home = None
-        berm = None
-        for tag in msg.detections:
-            if tag.id == 3:
-                home = tag
-            elif tag.id == 1:
-                berm = tag
-        # home = msg.detections[0]    # comment this out if you want to use the loop
-        if home is not None:
-            t = TransformStamped()
-            t.child_frame_id = "odom"
-            t.header.frame_id = 'map'
-            t.header.stamp = self.get_clock().now().to_msg()
-            self.makeTransforms(t, home)
+        # Can filter through the tags, but still only uses the first one.
+        # for tag in msg.detections:
+            # if tag.id == 3:
+            #     home = tag
 
-        if berm is not None:
-            m = TransformStamped()
-            m.child_frame_id = "map"
-            m.header.frame_id = 'other'
-            m.header.stamp = self.get_clock().now().to_msg()
+
+        tag = msg.detections[0]
+        t = TransformStamped()
+        t.child_frame_id = "odom"
+        t.header.frame_id = "map"
+        t.header.stamp = self.get_clock().now().to_msg()
+        self.makeTransforms(t, tag)
+        
+        # if home is not None:
+        #     t = TransformStamped()
+        #     t.child_frame_id = "odom"
+        #     t.header.frame_id = "map"
+        #     t.header.stamp = self.get_clock().now().to_msg()
+        #     self.makeTransforms(t, home)
+
+        # if berm is not None:
+        #     m = TransformStamped()
+        #     m.child_frame_id = "map"
+        #     m.header.frame_id = 'other'
+        #     m.header.stamp = self.get_clock().now().to_msg()
             
-            self.makeTransforms(m, berm)
+        #     self.makeTransforms(m, berm)
         # t.transform.translation.x = home.pose.pose.pose.position.x
         # t.transform.translation.y = home.pose.pose.pose.position.y
-        # # t.transform.translation.z = home.pose.pose.pose.position.z
+        # t.transform.translation.z = home.pose.pose.pose.position.z
         # t.transform.rotation.x = home.pose.pose.pose.orientation.x
         # t.transform.rotation.y = home.pose.pose.pose.orientation.y
         # t.transform.rotation.z = home.pose.pose.pose.orientation.z
@@ -74,12 +89,26 @@ class Apriltag(Node):
         
 
     def makeTransforms(self, t, tag):
-        t.transform.translation.x = tag.pose.pose.pose.position.x
-        t.transform.translation.y = tag.pose.pose.pose.position.z
-        t.transform.translation.z = tag.pose.pose.pose.position.y
-        t.transform.rotation.x = tag.pose.pose.pose.orientation.x
-        t.transform.rotation.y = tag.pose.pose.pose.orientation.y
-        t.transform.rotation.z = tag.pose.pose.pose.orientation.z
+        id = tag.id
+        tree = ET.parse(self.file_path)
+        root = tree.getroot()
+        
+        link = root[id - 1] # assumes tag 1 = home 1, tag 2 = home 2, 3 = berm 1 etc.
+
+        xyz_elements = link.findall(".//origin[@xyz]")
+        xyz_values = [element.attrib["xyz"] for element in xyz_elements]
+        xyz = xyz_values.split(" ")
+
+        rpy_elements = link.findall(".//origin[@rpy]")
+        rpy_values = [element.attrib["rpy"] for element in rpy_elements]
+        rpy = rpy_values.split(" ")
+
+        t.transform.translation.x = tag.pose.pose.pose.position.x - xyz[0]
+        t.transform.translation.y = tag.pose.pose.pose.position.z - xyz[1]
+        t.transform.translation.z = tag.pose.pose.pose.position.y - xyz[2]
+        t.transform.rotation.x = tag.pose.pose.pose.orientation.x - rpy[0]
+        t.transform.rotation.y = tag.pose.pose.pose.orientation.y - rpy[1]
+        t.transform.rotation.z = tag.pose.pose.pose.orientation.z - rpy[2]
         self.tf_broadcaster.sendTransform(t)
     
 
