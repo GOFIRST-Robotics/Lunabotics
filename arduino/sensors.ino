ad68192b50ae7824702cb242fc3d0156d4f4224c
Anthony Brogni <anthonybrog@gmail.com>
2024-01-03 13:48:07 -0600

    Implemented a new serial reader ROS node (#137)
    * Create publishers and serial reader
    
    * Change timer speed
    
    * Remove timer and use a while loop
    
    * Fix ardunio reading issue
    
    * Instead of using seperate topics, aggregate the sensor data into custom ROS msg types
    
    * fixed the order of array access (small bug)
    
    ---------
    
    Co-authored-by: Nathan <stang451@umn.edu>
    Co-authored-by: Nathan Stangler <123645803+NathanStangler@users.noreply.github.com>

--- arduino/sensors.ino
+++ arduino/sensors.ino
@@ -1,11 +1,11 @@
 // Define a struct to hold the sensor data
 struct SensorData {
-  int frontLeftEncoder;
-  int frontRightEncoder;
-  int backLeftEncoder;
-  int backRightEncoder;
   bool topLimitSwitch;
   bool bottomLimitSwitch;
+  uint16_t frontLeftEncoder;
+  uint16_t frontRightEncoder;
+  uint16_t backLeftEncoder;
+  uint16_t backRightEncoder;
 };
 
 // Define the sensor pins here
@@ -36,17 +36,17 @@ void loop() {
   SensorData data;
 
   // Read from the analog inputs (absolute encoders)
-  data.frontLeftEncoder = analogRead(FRONT_LEFT_ENCODER);
-  data.frontRightEncoder = analogRead(FRONT_RIGHT_ENCODER);
-  data.backLeftEncoder = analogRead(BACK_LEFT_ENCODER);
-  data.backRightEncoder = analogRead(BACK_RIGHT_ENCODER);
+  data.frontLeftEncoder = (uint16_t)analogRead(FRONT_LEFT_ENCODER);
+  data.frontRightEncoder = (uint16_t)analogRead(FRONT_RIGHT_ENCODER);
+  data.backLeftEncoder = (uint16_t)analogRead(BACK_LEFT_ENCODER);
+  data.backRightEncoder = (uint16_t)analogRead(BACK_RIGHT_ENCODER);
 
   // Read from the digital inputs (limit switches)
-  data.topLimitSwitch = digitalRead(TOP_LIMIT_SWITCH);
-  data.bottomLimitSwitch = digitalRead(BOTTOM_LIMIT_SWITCH);
+  data.topLimitSwitch = (bool)digitalRead(TOP_LIMIT_SWITCH);
+  data.bottomLimitSwitch = (bool)digitalRead(BOTTOM_LIMIT_SWITCH);
 
   // Send the struct over the serial bus to the Nvidia Jetson
-  Serial.write((byte *)&data, sizeof(data));
+  Serial.write((byte *)&data, sizeof(SensorData));
 
   // Wait 100ms before the next loop iteration
   delay(100);
