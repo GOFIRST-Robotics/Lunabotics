3f1b1676ae889191f3d8b05ddfaa14364c9c1be7
Anthony Brogni <anthonybrog@gmail.com>
2024-03-25 23:52:15 -0500

    Sync with main branch

--- src/gazebo/ros_gz_launch/launch/joy_gazebo.launch.py
+++ src/gazebo/ros_gz_launch/launch/joy_gazebo.launch.py
@@ -17,11 +17,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
 
@@ -30,10 +27,11 @@ def generate_launch_description():
     # Configure ROS nodes for launch
 
     # Setup project paths
-    pkg_project_bringup = get_package_share_directory('ros_gz_launch')
+    pkg_project_bringup = get_package_share_directory("ros_gz_launch")
 
     competition_field = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(pkg_project_bringup, 'launch', 'competition_field.launch.py')))
+        PythonLaunchDescriptionSource(os.path.join(pkg_project_bringup, "launch", "competition_field.launch.py"))
+    )
 
     rovr_control = Node(
         package="rovr_control",
@@ -53,8 +51,4 @@ def generate_launch_description():
         emulate_tty=True,
     )
 
-    return LaunchDescription([
-        competition_field,
-        rovr_control,
-        joy
-    ])
+    return LaunchDescription([competition_field, rovr_control, joy])
