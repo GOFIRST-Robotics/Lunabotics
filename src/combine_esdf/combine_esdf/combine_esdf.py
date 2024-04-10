600efe4d4e139bbfa726498e70d9ec890a8a9c76
alex berg <aaberg333@gmail.com>
2024-04-10 18:59:17 -0500

    Major clean up

--- src/combine_esdf/combine_esdf/combine_esdf.py
+++ src/combine_esdf/combine_esdf/combine_esdf.py
@@ -42,7 +42,7 @@ class combine_esdf(Node):
         
     
 
-        # Could eventually add more below ground costmaps here
+        # Could eventually add more below ground costmaps here...
         self.above_ground_costmap = None
         self.below_ground_costmap = None
         self.below_origin = None
@@ -51,69 +51,54 @@ class combine_esdf(Node):
         self.above_ground_pointcloud = None
         self.below_ground_pointcloud = None
 
+
+    # Not being used because it's too slow
     def sigmoid(self, x):
         return (2 / (1 + np.exp((x*-10) + 3)))
 
 
     def above_ground_callback(self, msg):
-        if (self.below_ground_costmap is None):
-            return
-        below_origin = self.below_origin
-        below_ground_costmap_one = np.copy(self.below_ground_costmap)
         if (len(msg.data) == 0):
             return
-        if (below_ground_costmap_one is None):
+        if (self.below_ground_costmap is None):
             self.costmap_publisher.publish(msg)
             return
+        
         self.above_ground_costmap = np.array(msg.data).reshape(msg.height, msg.width)
-        
-        
-        
-        
-        # condition = below_ground_costmap_one != 1000
-        # below_ground_costmap_one[condition] = 0
-
         origin = [msg.origin.x, msg.origin.y]
 
-        # print(origin, self.below_origin)
+        try:
+            # Calculating the points that align between the two costmaps.
+            offset_x1 = abs(int((origin[1] - self.below_origin[1]) / msg.resolution))
+            offset_y1 = abs(int((origin[0] - self.below_origin[0]) / msg.resolution))
+            offset_x2 = ((self.above_ground_costmap.shape[0] - self.below_ground_costmap.shape[0]) - offset_x1)
+            offset_y2 = ((self.above_ground_costmap.shape[1] - self.below_ground_costmap.shape[1]) - offset_y1)
+            
 
-        # self.above_ground_costmap[]
-        if (self.above_ground_costmap.shape[0] > below_ground_costmap_one.shape[0] and self.above_ground_costmap.shape[1] > below_ground_costmap_one.shape[1]):
-            try:
-                print("worked")
-                offset_x1 = abs(int((origin[1] - below_origin[1]) / msg.resolution))
-                offset_y1 = abs(int((origin[0] - below_origin[0]) / msg.resolution))
-                offset_x2 = ((self.above_ground_costmap.shape[0] - below_ground_costmap_one.shape[0]) - offset_x1)
-                offset_y2 = ((self.above_ground_costmap.shape[1] - below_ground_costmap_one.shape[1]) - offset_y1)
-                
-
-                # To invert or not to invert, that is the question.
-                # Comment this next chunk out if the lower esdf is only showing the hole as red, not the terrain.
-                # below_ground_costmap_one[below_ground_costmap_one == 1000] = 0
-                # below_ground_costmap_one = np.full(below_ground_costmap_one.shape, 2) - below_ground_costmap_one
-                # below_ground_costmap_one = 2 - self.sigmoid(below_ground_costmap_one)
+            # To invert or not to invert, that is the question. 
+            # Comment this next chunk out if the lower esdf is only showing the hole as red, not the terrain.
+            # this actually doesn't work yet, so leave it commented.
+            # self.below_ground_costmap[self.below_ground_costmap == 1000] = 0
+            # self.below_ground_costmap = np.full(self.below_ground_costmap.shape, 2) - self.below_ground_costmap
+            # self.below_ground_costmap = 2 - self.sigmoid(self.below_ground_costmap)
 
 
-                # if you don't want to invert, uncomment this:
-                condition2 = below_ground_costmap_one <=2
-                below_ground_costmap_one[condition2] = 0
+            # if you don't want to invert, uncomment this:
+            condition2 = self.below_ground_costmap <= 2
+            self.below_ground_costmap[condition2] = 0
 
 
-                # Actually combine the costmaps.
-                self.above_ground_costmap[offset_x1:-offset_x2, offset_y1:-offset_y2] = np.minimum((self.above_ground_costmap[offset_x1:-offset_x2, offset_y1:-offset_y2]), below_ground_costmap_one)
-        
-                # Blur the costmap, so that the hole is less abrupt        
-                self.above_ground_costmap = gaussian_filter(self.above_ground_costmap, sigma=5)
+            # Actually combine the costmaps.
+            self.above_ground_costmap[offset_x1:-offset_x2, offset_y1:-offset_y2] = np.minimum((self.above_ground_costmap[offset_x1:-offset_x2, offset_y1:-offset_y2]), self.below_ground_costmap)
+    
+            # Blur the costmap, so that the hole is less abrupt        
+            self.above_ground_costmap = gaussian_filter(self.above_ground_costmap, sigma=5)
 
 
-                msg.data = self.above_ground_costmap.flatten().tolist()
-                self.costmap_publisher.publish(msg)
-            except:
-                print("did not work")
-        else:
-            pass
-        if (self.above_ground_costmap.size < below_ground_costmap_one.size):
-            print("shit")
+            msg.data = self.above_ground_costmap.flatten().tolist()
+            self.costmap_publisher.publish(msg)
+        except:
+            print("did not work")
 
 
     # Easily replicable for more below ground costmaps. Stack the costmaps into a 3D array
@@ -123,7 +108,6 @@ class combine_esdf(Node):
             return   
         self.below_ground_costmap = np.array(msg.data).reshape(msg.height, msg.width)
         self.below_origin = [msg.origin.x, msg.origin.y]
-        # print(msg.resolution)
 
 
 
@@ -131,6 +115,7 @@ class combine_esdf(Node):
     # Performance is garbage, inadvisable to run during comps
     def above_ground_pointcloud_callback(self, msg):
         if (len(msg.data) == 0 or self.below_ground_pointcloud is None):
+            self.pointcloud_publisher.publish(msg)
             return
         above_ground_float_values = np.frombuffer(msg.data, dtype=np.uint8).view(dtype=np.float32)
         below_ground_cloud = self.below_ground_pointcloud.view(dtype=np.float32)
@@ -182,55 +167,3 @@ def main(args=None):
 
     node.destroy_node()
     rclpy.shutdown()
-
-
-
-
-
-
-
-
-
-
-
-# print(offset_x1, offset_x2, self.above_ground_costmap.shape, below_ground_costmap_one.shape)
-                # print(offset_x1, offset_x2, offset_y1, offset_y2)
-                
-                # print(np.average(below_ground_costmap_one), np.amin(below_ground_costmap_one), np.amax(below_ground_costmap_one))
-                
-                # below_ground_costmap_one = np.flip(below_ground_costmap_one, axis=1)
-                # self.above_ground_costmap[offset_x2:-offset_x1, offset_y1:-offset_y2] = 1000
-            
-
-
-    # print(np.average(below_ground_costmap_one), np.amin(below_ground_costmap_one), np.amax(below_ground_costmap_one))
-        # inverse_below_ground_costmap = np.full(below_ground_costmap_one.shape, 2) - below_ground_costmap_one
-
-        # # pad the below ground costmap, to give it the same dimensions as the above ground costmap        
-        # max_width = max(self.above_ground_costmap.shape[0], inverse_below_ground_costmap.shape[0])
-        # max_height = max(self.above_ground_costmap.shape[1], inverse_below_ground_costmap.shape[1])
-        # inverse_below_ground_costmap = np.pad(inverse_below_ground_costmap, ((max_width - inverse_below_ground_costmap.shape[0], 0), (max_height - inverse_below_ground_costmap.shape[1], 0)), 'constant', constant_values=(2))
-        # self.above_ground_costmap = np.pad(self.above_ground_costmap, ((max_width - self.above_ground_costmap.shape[0], 0), (max_height - self.above_ground_costmap.shape[1], 0)), 'constant', constant_values=(2))
-
-        # # Create a message to publish the combined costmap
-        # message = DistanceMapSlice()
-        # message = msg # should be largely identical to the original message.
-        # if self.above_ground_costmap.shape == inverse_below_ground_costmap.shape:
-        #     inverse_below_ground_costmap = inverse_below_ground_costmap * 1000
-        #     message.data = np.maximum(inverse_below_ground_costmap, self.above_ground_costmap).flatten().tolist()
-        #     self.costmap_publisher.publish(message)
-
-
-
-         # self.above_ground_costmap[np.where(self.above_ground_costmap == 0)] = 1000
-                
-                # self.above_ground_costmap = np.flip(self.above_ground_costmap, axis=1)
-                # self.above_ground_costmap[0:offset_x2, 0:5] = 0
-                # self.above_ground_costmap[0:10, 0:offset_y1] = 0
-                # self.above_ground_costmap[-offset_x1:-1, -10:] = 0
-                # self.above_ground_costmap[-10:, -offset_y2:-1] = 0
-                # image2 = Image.fromarray(below_ground_costmap_one.astype('uint8'))
-                # image2.save("numpy_matrix_image2.png")
-
-                # image = Image.fromarray(self.above_ground_costmap.astype('uint8'))
-                # image.save("numpy_matrix_image.png")
