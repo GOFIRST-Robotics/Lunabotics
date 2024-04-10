96dfdcb9da5b3264fe6c50f6d8dbe36c6de25439
Isopod00 <anthonybrog@gmail.com>
2024-04-09 22:17:23 -0500

    Create laptop_launch.py

---
+++ src/rovr_control/launch/laptop_launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+# Launches the joystick node and the gstreamer camera client node on the team laptop
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    joystick_node = Node(
+        package="joy",
+        executable="joy_node",
+        parameters=["config/joy_node.yaml"],
+    )
+
+    GStreamer_node = Node(
+        package="gstreamer",
+        executable="server",
+    )
+
+    ld.add_action(joystick_node)
+    ld.add_action(GStreamer_node)
+
+    return ld
+
+
+# TODO: Move this file into the GStreamer package
