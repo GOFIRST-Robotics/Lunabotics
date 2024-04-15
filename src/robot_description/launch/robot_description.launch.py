d3d82d924a73a199edc87999abf91eea855639e7
Jonathan Blixt <jmblixt3@gmail.com>
2024-04-14 20:21:47 -0500

    Utilize zed xarco in robot description

--- src/robot_description/launch/robot_description.launch.py
+++ src/robot_description/launch/robot_description.launch.py
@@ -1,25 +1,31 @@
 from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.descriptions import ParameterValue
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def generate_launch_description():
 
     pkg_project_description = get_package_share_directory("robot_description")
-        # Load the SDF file from "description" package
-    urdf_file = os.path.join(pkg_project_description, "models", "master_ASM", "urdf", "master_ASM.urdf")
-    with open(urdf_file, "r") as infp:
-        robot_desc = infp.read()
-    
-    robot_state_publisher = Node(
-    package="robot_state_publisher",
-    executable="robot_state_publisher",
-    name="robot_state_publisher",
-    output="both",
-    parameters=[
-        {"use_sim_time": True},
-        {"robot_description": robot_desc},
-    ],
+    # Load the SDF file from "description" package
+    xacro_path = os.path.join(
+        pkg_project_description, "models", "master_ASM", "urdf", "master_ASM.urdf.xarco"
     )
-    
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[
+            {
+                "robot_description": ParameterValue(
+                    Command(["xacro", " ", xacro_path]), value_type=str
+                )
+            }
+        ],
+    )
+
     return LaunchDescription([robot_state_publisher])
