73883f26e49b9f6b69492c1c870c5ad54e36fe35
Jonathan Blixt <jmblixt3@gmail.com>
2024-04-28 17:29:57 -0500

    Hotfix gazebo parameters The new nvblox parameter was hard coded in gazebo making it impossible to change at runtime, this was fixed

--- src/isaac_ros/isaac_ros_launch/launch/gazebo_launch.py
+++ src/isaac_ros/isaac_ros_launch/launch/gazebo_launch.py
@@ -1,20 +1,27 @@
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription,DeclareLaunchArgument
 from launch import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
 import os
 
 
 def generate_launch_description():
     bringup_dir = get_package_share_directory("isaac_ros_launch")
 
+    use_nvblox_arg = DeclareLaunchArgument(
+        "use_nvblox",
+        default_value="False",
+        description="Whether to run nvblox",
+    )
+
     ld = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(bringup_dir, "isaac_launch.py")),
         launch_arguments={
             "setup_for_zed": "False",
             "setup_for_gazebo": "True",
-            "use_nvblox": "False",
+            "use_nvblox": LaunchConfiguration("use_nvblox"),
         }.items(),
     )
 
-    return LaunchDescription([ld])
+    return LaunchDescription([use_nvblox_arg,ld])
