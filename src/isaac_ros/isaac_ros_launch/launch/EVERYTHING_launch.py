d54662693fc7f59563f557cccb11d569921623e1
Anthony Brogni <anthonybrog@gmail.com>
2024-05-02 22:18:11 -0500

    Added commented sections

--- src/isaac_ros/isaac_ros_launch/launch/EVERYTHING_launch.py
+++ src/isaac_ros/isaac_ros_launch/launch/EVERYTHING_launch.py
@@ -1,5 +1,6 @@
 import os
 
+# from launch_ros.actions import Node  # NOTE: Uncomment this code block if we get the gstreamer server working
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -11,7 +12,7 @@ def generate_launch_description():
 
     main_bringup_dir = get_package_share_directory("rovr_control")
     isaac_bringup_dir = get_package_share_directory("isaac_ros_launch")
-    check_load_bringup_dir = get_package_share_directory("skimmer")
+    # check_load_bringup_dir = get_package_share_directory("skimmer") # NOTE: Uncomment this line if we decide to actually use ros_check_load
 
     main_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(main_bringup_dir, "main_launch_no_joysticks.py")),
@@ -28,16 +29,24 @@ def generate_launch_description():
         }.items(),
     )
 
-    # TODO: check_load has not been tested yet!
-    check_load_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(check_load_bringup_dir, "check_load_launch.py")),
-    )
+    # NOTE: Uncomment this code block if we decide to actually use ros_check_load
+    # check_load_launch = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(os.path.join(check_load_bringup_dir, "check_load_launch.py")),
+    # )
 
-    # TODO: Add the gstreamer server node when that is ready
+    # NOTE: Uncomment this code block if we get the gstreamer server working
+    # gstreamer_server = Node(
+    #     package="gstreamer",
+    #     executable="server_node",
+    #     name="gstreamer_server_node",
+    #     output="screen",
+    #     emulate_tty=True,
+    # )
 
     # Add all of the actions to the launch description
     ld.add_action(main_launch)
-    ld.add_action(check_load_launch)
+    # ld.add_action(check_load_launch) # NOTE: Uncomment this line if we decide to actually use ros_check_load
+    # ld.add_action(gstreamer_server)  # NOTE: Uncomment this line if we get the gstreamer server working
     ld.add_action(isaac_launch)
     # Return the launch description
     return ld
