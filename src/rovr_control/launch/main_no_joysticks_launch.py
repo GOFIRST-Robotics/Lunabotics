d1824e61bb8a8a21339016a952ce46ed4903851c
Anthony Brogni <anthonybrog@gmail.com>
2024-08-21 23:13:38 -0500

    Added the action servers back to the rovr_control launch files

--- src/rovr_control/launch/main_no_joysticks_launch.py
+++ src/rovr_control/launch/main_no_joysticks_launch.py
@@ -52,11 +52,31 @@ def generate_launch_description():
         name="ros2socketcan",
     )
 
+    # Add our autonomous action servers here
+    calibrate_field_coordinate_server = Node(
+        package="rovr_control",
+        executable="calibrate_field_coordinate_server",
+        name="calibrate_field_coordinate_server",
+    )
+    auto_dig_server = Node(
+        package="rovr_control",
+        executable="auto_dig_server",
+        name="auto_dig_server",
+    )
+    auto_offload_server = Node(
+        package="rovr_control",
+        executable="auto_offload_server",
+        name="auto_offload_server",
+    )
+
     ld.add_action(rovr_control)
     ld.add_action(motor_control)
     ld.add_action(drivetrain)
     ld.add_action(skimmer)
     ld.add_action(read_serial)
     ld.add_action(can_bus)
+    ld.add_action(calibrate_field_coordinate_server)
+    ld.add_action(auto_dig_server)
+    ld.add_action(auto_offload_server)
 
     return ld
