8b447e959982d4191062a27a53511e86d1b4e6d3
Jonathan Blixt <jmblixt3@gmail.com>
2024-08-30 21:27:17 +0000

    Make action utilize async await syntax Replace all rclpy.spin_until loops with await. In a few places this wasn't possible so a custom future callback was used. The only remaining spin_once is for motor ramp up/ down which is set to be implemented soon Also removed minor deprecated devcontainer config for python language server
    Signed-off-by: Jonathan Blixt <jmblixt3@gmail.com>

--- src/rovr_control/rovr_control/calibrate_field_coordinate_server.py
+++ src/rovr_control/rovr_control/calibrate_field_coordinate_server.py
@@ -1,9 +1,9 @@
 import math
 
 import rclpy
-from rclpy.action import ActionClient, ActionServer
+from rclpy.action import ActionClient, ActionServer, CancelResponse
 from rclpy.action.client import ClientGoalHandle
-from rclpy.action.server import CancelResponse
+from rclpy.action.server import ServerGoalHandle
 from rovr_interfaces.action import CalibrateFieldCoordinates
 from rovr_interfaces.srv import ResetOdom
 from rclpy.node import Node
@@ -23,18 +23,15 @@ class CalibrateFieldCoordinateServer(Node):
             self.execute_callback,
             cancel_callback=self.cancel_callback,
         )
-        self.client_node = rclpy.create_node("calibrate_field_action_server_client")
-        self.cli_set_apriltag_odometry = self.client_node.create_client(ResetOdom, "resetOdom")
+        self.cli_set_apriltag_odometry = self.create_client(ResetOdom, "resetOdom")
         self.future_odom = Future()
 
-        self.cli_spin = ActionClient(self.client_node, Spin, "spin")
+        self.cli_spin = ActionClient(self, Spin, "spin")
         self.spin_handle = ClientGoalHandle(None, None, None)
 
-    def execute_callback(self, goal_handle: CalibrateFieldCoordinates.Goal):
+    async def execute_callback(self, goal_handle: ServerGoalHandle):
         """This method rotates until we can see apriltag(s) and then sets the map -> odom tf."""
-        self.get_logger().info("Executing goal...")
         result = CalibrateFieldCoordinates.Result()
-        self.get_logger().info("Beginning search for apriltags")
 
         # Make sure the services are available
         if not self.cli_set_apriltag_odometry.wait_for_service(timeout_sec=1.0):
@@ -48,32 +45,16 @@ class CalibrateFieldCoordinateServer(Node):
 
         # Spin around to find the apriltag
         spin_goal = Spin.Goal(target_yaw=2 * math.pi)
-        spin_request = self.cli_spin.send_goal_async(spin_goal)
-        rclpy.spin_until_future_complete(self.client_node, spin_request)
-        self.spin_handle: ClientGoalHandle = spin_request.result()
-        if not self.spin_handle.accepted:
-            self.get_logger().error("Spin request was rejected")
-            goal_handle.abort()
-            return result
+        self.spin_handle: ClientGoalHandle = await self.cli_spin.send_goal_async(spin_goal)
         future_spin: Future = self.spin_handle.get_result_async()
         self.future_odom = Future()
 
-        while (
-            not self.future_odom.cancelled()
-            and not (self.future_odom.done() and self.future_odom.result().success is True)
-            and not future_spin.done()
-        ):
+        while not future_spin.done():
             self.future_odom = self.cli_set_apriltag_odometry.call_async(ResetOdom.Request())
-            rclpy.spin_until_future_complete(self.client_node, self.future_odom)
+            if (await self.future_odom).success is True:
+                await self.spin_handle.cancel_goal_async()
 
-        if self.spin_handle.status == GoalStatus.STATUS_CANCELED:
-            self.get_logger().warn("Spin canceled")
-            goal_handle.canceled()
-            return result
-        elif self.future_odom.done() and self.future_odom.result().success is True:
-            self.get_logger().info("map -> odom TF published!")
-            rclpy.spin_until_future_complete(self.client_node, self.spin_handle.cancel_goal_async())
-        else:
+        if self.spin_handle.status == GoalStatus.STATUS_SUCCEEDED:
             self.get_logger().warn("Failed to find apriltag")
             goal_handle.abort()
             return result
@@ -83,27 +64,18 @@ class CalibrateFieldCoordinateServer(Node):
             goal_handle.abort()
             return result
         spin_goal = Spin.Goal(target_yaw=math.pi)
-        spin_request = self.cli_spin.send_goal_async(spin_goal)
-        rclpy.spin_until_future_complete(self.client_node, spin_request)
-        self.spin_handle: ClientGoalHandle = spin_request.result()
-        if not self.spin_handle.accepted:
-            self.get_logger().error("Spin request was rejected")
-            goal_handle.abort()
-            return result
-        rclpy.spin_until_future_complete(self.client_node, self.spin_handle.get_result_async())
-        if goal_handle.status == GoalStatus.STATUS_CANCELING:
-            self.get_logger().warn("Final spin to field Spin canceled")
-            goal_handle.canceled()
-            return result
+        self.spin_handle: ClientGoalHandle = await self.cli_spin.send_goal_async(spin_goal)
+        await self.spin_handle.get_result_async()
         goal_handle.succeed()
         return result
 
-    def cancel_callback(self, cancel_request):
+    def cancel_callback(self, cancel_request: ServerGoalHandle):
         """This method is called when the action is canceled."""
-        self.get_logger().info("Goal was canceled")
+        self.get_logger().info("Goal is cancelling")
         if self.spin_handle.status == GoalStatus.STATUS_EXECUTING:
             self.spin_handle.cancel_goal_async()
-        self.future_odom.cancel()
+        if not self.future_odom.done():
+            self.future_odom.cancel()
         return CancelResponse.ACCEPT
 
 
