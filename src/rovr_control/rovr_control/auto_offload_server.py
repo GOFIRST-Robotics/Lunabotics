29e72f24afbb475b0d8464cb29debe07543f893f
Jonathan Blixt <jmblixt3@gmail.com>
2024-08-30 21:27:17 +0000

    Make action utilize async await syntax Replace all rclpy.spin_until loops with await. In a few places this wasn't possible so a custom future callback was used. The only remaining spin_once is for motor ramp up/ down which is set to be implemented soon Also removed minor deprecated devcontainer config for python language server
    Signed-off-by: Jonathan Blixt <jmblixt3@gmail.com>

--- src/rovr_control/rovr_control/auto_offload_server.py
+++ src/rovr_control/rovr_control/auto_offload_server.py
@@ -1,41 +1,40 @@
 import rclpy
 from rclpy.action import ActionServer
-from rclpy.node import Node
-
-from rclpy.action.server import CancelResponse
+from rclpy.action.server import CancelResponse, ServerGoalHandle, GoalStatus
 from std_msgs.msg import Bool
 
 from rovr_interfaces.action import AutoOffload
 from rovr_interfaces.srv import Drive, Stop, SetPosition, SetPower
 
-import time
+from rovr_control.node_util import AsyncNode
 
 
-class AutoOffloadServer(Node):
+class AutoOffloadServer(AsyncNode):
     def __init__(self):
         super().__init__("auto_offload_server")
         self._action_server = ActionServer(
-            self, AutoOffload, "auto_offload", self.execute_callback, cancel_callback=self.cancel_callback
+            self,
+            AutoOffload,
+            "auto_offload",
+            self.execute_callback,
+            cancel_callback=self.cancel_callback,
         )
-        self.client_node = rclpy.create_node("auto_offload_action_server_client")
 
         # TODO: This should not be needed anymore after ticket #257 is implemented!
-        self.skimmer_goal_subscription = self.client_node.create_subscription(
+        self.skimmer_goal_subscription = self.create_subscription(
             Bool, "/skimmer/goal_reached", self.skimmer_goal_callback, 10
         )
 
-        self.cli_drivetrain_drive = self.client_node.create_client(Drive, "drivetrain/drive")
-        self.cli_drivetrain_stop = self.client_node.create_client(Stop, "drivetrain/stop")
+        self.cli_drivetrain_drive = self.create_client(Drive, "drivetrain/drive")
+        self.cli_drivetrain_stop = self.create_client(Stop, "drivetrain/stop")
+        # TODO: This should not be updated to used #257
+        self.cli_lift_setPosition = self.create_client(SetPosition, "lift/setPosition")
+        self.cli_lift_stop = self.create_client(Stop, "lift/stop")
 
-        self.cli_lift_setPosition = self.client_node.create_client(SetPosition, "lift/setPosition")
-        self.cli_lift_stop = self.client_node.create_client(Stop, "lift/stop")
+        self.cli_skimmer_setPower = self.create_client(SetPower, "skimmer/setPower")
+        self.cli_skimmer_stop = self.create_client(Stop, "skimmer/stop")
 
-        self.cli_skimmer_setPower = self.client_node.create_client(SetPower, "skimmer/setPower")
-        self.cli_skimmer_stop = self.client_node.create_client(Stop, "skimmer/stop")
-
-        self.canceled = False
-
-    def execute_callback(self, goal_handle: AutoOffload.Goal):
+    async def execute_callback(self, goal_handle: ServerGoalHandle):
         """This method lays out the procedure for autonomously offloading!"""
         self.get_logger().info("Starting Autonomous Offload Procedure!")
         result = AutoOffload.Result()
@@ -70,63 +69,42 @@ class AutoOffloadServer(Node):
         self.cli_drivetrain_drive.call_async(
             Drive.Request(forward_power=0.0, horizontal_power=-0.25, turning_power=0.0)
         )
-        time_to_drive = 10e9
-        start_time = self.get_clock().now().nanoseconds
+
         self.get_logger().info("Auto Driving")
-        while self.get_clock().now().nanoseconds - start_time < time_to_drive:
-            rclpy.spin_once(self.client_node, timeout_sec=0)  # Allows for task to be canceled
-            if self.canceled:
-                self.get_logger().warn("Driving Procedure Canceled!\n")
-                self.canceled = False
-                goal_handle.canceled()
-                return result
-            time.sleep(0.1)
-        self.cli_drivetrain_stop.call_async(Stop.Request())
+        # drive for 10 seconds
+        await self.async_sleep(10)  # Allows for task to be canceled
+        await self.cli_drivetrain_stop.call_async(Stop.Request())
 
         # Raise up the skimmer in preparation for dumping
         self.get_logger().info("Raising the Lift!")
         self.cli_lift_setPosition.call_async(SetPosition.Request(position=goal_handle.request.lift_dumping_position))
         # Wait for the lift goal to be reached
-        self.skimmer_goal_reached = False
-        while not self.skimmer_goal_reached:
-            self.get_logger().info("Moving the lift to the goal")
-            rclpy.spin_once(self.client_node, timeout_sec=0)  # Allows for task to be canceled
-            if self.canceled:
-                self.get_logger().warn("Raising the Lift was Canceled!\n")
-                self.canceled = False
-                goal_handle.canceled()
-                return result
-            time.sleep(0.1)
+        await self.skimmer_sleep()
+        if goal_handle.status != GoalStatus.STATUS_CANCELING:
+            self.get_logger().info("Cancelling")
+            return
 
+        self.get_logger().info("Offloading")
         self.cli_skimmer_setPower.call_async(SetPower.Request(power=goal_handle.request.skimmer_belt_power))
-        time_to_offload = 1.0 / abs(goal_handle.request.skimmer_belt_power) * 1e9  # convert from seconds to nanoseconds
-        start_time = self.get_clock().now().nanoseconds
-        self.get_logger().info("Commence Offloading!")
-        while self.get_clock().now().nanoseconds - start_time < time_to_offload:
-            rclpy.spin_once(self.client_node, timeout_sec=0)  # Allows for task to be canceled
-            if self.canceled:
-                self.get_logger().warn("Offload Belt was Canceled!\n")
-                self.canceled = False
-                goal_handle.canceled()
-                return result
-            time.sleep(0.1)
+        # sleep for the amount of time it takes to offload
+        time_to_offload = 1.0 / abs(goal_handle.request.skimmer_belt_power)
+        await self.async_sleep(time_to_offload)  # Allows for task to be canceled
 
-        self.cli_skimmer_stop.call_async(Stop.Request())  # Stop the skimmer belt
-
-        self.get_logger().info("Autonomous Offload Procedure Complete!\n")
+        await self.cli_skimmer_stop.call_async(Stop.Request())  # Stop the skimmer belt
+        self.get_logger().info("Autonomous Offload Procedure Complete!")
         goal_handle.succeed()
         return result
 
-    def cancel_callback(self, cancel_request):
+    def cancel_callback(self, cancel_request: ServerGoalHandle):
         """This method is called when the action is canceled."""
-        self.get_logger().info("Goal was canceled")
-        self.canceled = True
+        self.get_logger().info("Goal is cancelling")
+        # If lift is raising stop it
+        if not self.skimmer_goal_reached.done():
+            self.cli_drivetrain_stop.call_async(Stop.Request())
+        if super().cancel_callback(cancel_request) == CancelResponse.ACCEPT:
+            self.cli_drivetrain_stop.call_async(Stop.Request())
+            self.cli_skimmer_stop.call_async(Stop.Request())
         return CancelResponse.ACCEPT
-
-    # TODO: This should not be needed anymore after ticket #257 is implemented!
-    def skimmer_goal_callback(self, msg: Bool) -> None:
-        """Update the member variable accordingly."""
-        self.skimmer_goal_reached = msg.data
 
 
 def main(args=None) -> None:
