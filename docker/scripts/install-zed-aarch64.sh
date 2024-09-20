c734698d4c7a3192b70a4c61f6df4b02731fadf4
Jonathan Blixt <jmblixt3@gmail.com>
2024-09-20 16:20:40 -0500

    Initial attempt at isaac ros 3

---
+++ docker/scripts/install-zed-aarch64.sh
@@ -0,0 +1,20 @@
+# Based on https://github.com/stereolabs/zed-docker
+
+# Download dependencies for zed SDK installation RUN file
+sudo apt-get update -y || true
+sudo apt-get install --no-install-recommends lsb-release wget less zstd udev sudo apt-transport-https -y
+
+# Download zed SDK installation RUN file to /tmp directory
+cd /tmp
+# https://download.stereolabs.com/zedsdk/4.1/l4t36.3/jetsons
+wget -q --no-check-certificate -O ZED_SDK_Linux.run https://download.stereolabs.com/zedsdk/${ZED_SDK_MAJOR}.${ZED_SDK_MINOR}/l4t36.3/jetsons
+chmod +x ZED_SDK_Linux.run ; ./ZED_SDK_Linux.run silent skip_od_module skip_python skip_drivers
+
+# Symlink required to use the streaming features on Jetson inside a container, based on
+# https://github.com/stereolabs/zed-docker/blob/fd514606174d8bb09f21a229f1099205b284ecb6/4.X/l4t/devel/Dockerfile#L27C5-L27C95
+sudo ln -sf /usr/lib/aarch64-linux-gnu/tegra/libv4l2.so.0 /usr/lib/aarch64-linux-gnu/libv4l2.so
+
+# Cleanup
+sudo rm -rf /usr/local/zed/resources/*
+rm -rf ZED_SDK_Linux.run
+sudo rm -rf /var/lib/apt/lists/*
--- scripts/enter_isaac_ros_container.sh
+++ scripts/enter_isaac_ros_container.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-printf "CONFIG_IMAGE_KEY=ros2_humble.realsense.deepstream.user.zed.umn.gazebo \n" > ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/.isaac_ros_common-config && \
-echo "-v /usr/local/zed/resources:/usr/local/zed/resources -v /ssd:/ssd -v /usr/local/zed/settings:/usr/local/zed/settings" > ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/.isaac_ros_dev-dockerargs && \
-cd ~/Lunabotics/src/isaac_ros/isaac_ros_common/docker && \
-../scripts/run_dev.sh ~/Lunabotics
+docker_arg="-v /usr/local/zed/resources:/usr/local/zed/resources -v /ssd:/ssd -v /usr/local/zed/settings:/usr/local/zed/settings"
+bash ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/run_dev.sh -d ~/Lunabotics -i ros2_humble.realsense.deepstream.user.zed.umn.gazebo -a "${docker_arg}" -v
