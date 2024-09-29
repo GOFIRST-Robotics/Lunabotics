99329e7ce7eb7d3ac7a92b64d541afa929674413
Jonathan Blixt <jmblixt3@gmail.com>
2024-09-29 18:52:21 -0500

    Move deepstream dependency into base image

--- scripts/enter_isaac_ros_container.sh
+++ scripts/enter_isaac_ros_container.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 printf 'CONFIG_DOCKER_SEARCH_DIRS="$HOME/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/../../../../docker"\n'> ~/.isaac_ros_common-config
+image_key="ros2_humble.realsense.user.zed1.umn.gazebo"
 docker_arg="-v /usr/local/zed/resources:/usr/local/zed/resources -v /ssd:/ssd -v /usr/local/zed/settings:/usr/local/zed/settings"
-bash ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/run_dev.sh -d ~/Lunabotics -i "ros2_humble.realsense.deepstream.user.zed1.umn.gazebo" -a "${docker_arg}" -v
+bash ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/build_image_layers.sh -b nvcr.io/nvidia/deepstream:7.0-triton-multiarch -i "${image_key}"
+bash ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/run_dev.sh -d ~/Lunabotics -i "${image_key} -a "${docker_arg}" -v
