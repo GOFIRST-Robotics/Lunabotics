20efe1ef7ae896bc40ac3b3d1e8e48b5bf9a1684
Anthony Brogni <anthonybrog@gmail.com>
2024-10-01 15:57:21 -0500

    Added our missing user Dockerfile

--- scripts/enter_isaac_ros_container.sh
+++ scripts/enter_isaac_ros_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 printf 'CONFIG_DOCKER_SEARCH_DIRS="$HOME/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/../../../../docker"\n'> ~/.isaac_ros_common-config
-image_key="ros2_humble.realsense.deepstream.user.zed1.umn.gazebo"
+image_key="ros2_humble.realsense.deepstream.user1.zed1.umn.gazebo"
 docker_arg="-v /usr/local/zed/resources:/usr/local/zed/resources -v /ssd:/ssd -v /usr/local/zed/settings:/usr/local/zed/settings"
 if [ "$(uname -m)" = "x86_64" ]; then
     ARCH=amd64
@@ -8,6 +8,6 @@ else
     ARCH=amd64
 fi
 if [ ! -f deepstream/*/*${ARCH}.deb ]; then
-    ngc registry resource download-version nvidia/deepstream:7.0 --dest 'deepstream' --file "*${ARCH}*" || (echo install ngc && exit 1)
+    ngc registry resource download-version nvidia/deepstream:7.0 --dest 'deepstream' --file "*${ARCH}*" || (echo You need to install ngc! && exit 1)
 fi
 bash ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/run_dev.sh -d ~/Lunabotics -i "${image_key}" -a "${docker_arg}" -v
