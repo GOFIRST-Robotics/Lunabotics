53fa44268d6633f5edf9fb7bbaec0529bda7abd3
Jonathan Blixt <jmblixt3@gmail.com>
2024-10-03 17:41:38 -0500

    Readd docker_args

--- scripts/enter_isaac_ros_container.sh
+++ scripts/enter_isaac_ros_container.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 printf 'CONFIG_DOCKER_SEARCH_DIRS="$HOME/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/../../../../docker"\n'> ~/.isaac_ros_common-config
 image_key="ros2_humble.realsense.deepstream.user.zed.umn.gazebo"
+docker_arg="-v /usr/local/zed/resources:/usr/local/zed/resources -v /ssd:/ssd -v /usr/local/zed/settings:/usr/local/zed/settings"
 
 bash ~/Lunabotics/scripts/build_image.sh
-
-echo "-v /usr/local/zed/resources:/usr/local/zed/resources -v /ssd:/ssd -v /usr/local/zed/settings:/usr/local/zed/settings" > ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/.isaac_ros_dev-dockerargs && \
 
 bash ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/run_dev.sh -d ~/Lunabotics -i "${image_key}" -a "${docker_arg}" -v -b
