12497771a3f57e911f0d7155ec02f86f43df94f1
umnrobotics <robotics@umn.edu>
2024-10-09 18:29:19 -0500

    change to deepstream download, so it downloads arm64 on the jetson

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -3,10 +3,6 @@ if [ -z "${image_key}"]; then
     image_key="ros2_humble.realsense.deepstream.user.zed.umn.gazebo"
 fi
 
-if [ ! -f ${HOME}/Lunabotics/docker/deepstream/*/deepstream*.deb ]; then
-    ngc registry resource download-version nvidia/deepstream:7.0 --dest "${HOME}/Lunabotics/docker/deepstream" --file "*.deb" || echo You need to install ngc!
-fi
-
 declare -a arr=("$(uname -m)")
 
 for PLAT in "${arr[@]}"
@@ -16,6 +12,10 @@ if [ "${PLAT}" = "x86_64" ]; then
     ARCH=amd64
 else
     ARCH=arm64
+fi
+
+if [ ! -f ${HOME}/Lunabotics/docker/deepstream/*/deepstream*.deb ]; then
+    ngc registry resource download-version nvidia/deepstream:7.0 --dest "${HOME}/Lunabotics/docker/deepstream" --file "*${ARCH}.deb" || echo You need to install ngc!
 fi
 
 find "${HOME}/Lunabotics/docker/deepstream" -name *${ARCH}.deb -type f | xargs -I f mv f ${HOME}/Lunabotics/docker/deepstream/deepstream.deb
