c63a14ecba20b5b54ebc326a9b185acc26b42e88
Anthony Brogni <anthonybrog@gmail.com>
2024-10-22 18:25:31 -0500

    Sync branch with main

--- scripts/enter_isaac_ros_container.sh
+++ scripts/enter_isaac_ros_container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+printf 'CONFIG_DOCKER_SEARCH_DIRS="$HOME/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/../../../../docker"\n BASE_DOCKER_REGISTRY_NAMES="umnrobotics/isaac_ros3.1"\n'> ~/.isaac_ros_common-config
+image_key="ros2_humble.realsense.deepstream.user.zed.umn.gazebo"
+docker_arg="-v /usr/local/zed/resources:/usr/local/zed/resources -v $HOME/rosbags:/rosbags -v /usr/local/zed/settings:/usr/local/zed/settings"
 
-printf "CONFIG_IMAGE_KEY=ros2_humble.realsense.deepstream.user.zed.umn.gazebo \n" > ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/.isaac_ros_common-config && \
-echo "-v /usr/local/zed/resources:/usr/local/zed/resources -v /ssd:/ssd -v /usr/local/zed/settings:/usr/local/zed/settings" > ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/.isaac_ros_dev-dockerargs && \
-cd ~/Lunabotics/src/isaac_ros/isaac_ros_common/docker && \
-../scripts/run_dev.sh ~/Lunabotics
+bash ~/Lunabotics/scripts/build_image.sh
+
+bash ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/run_dev.sh -d ~/Lunabotics -i "${image_key}" -a "${docker_arg}" -v -b
