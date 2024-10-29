7282bede494c4ae49cd3a0076be066c1e2a59c1a
Isopod00 <anthonybrog@gmail.com>
2024-10-29 22:16:41 +0000

    Make use of the built-in Trigger service type

--- src/digger/digger/digger_node.py
+++ src/digger/digger/digger_node.py
@@ -12,8 +12,9 @@ from std_msgs.msg import Bool
 
 # Import custom ROS 2 interfaces
 from rovr_interfaces.srv import MotorCommandSet, MotorCommandGet
-from rovr_interfaces.srv import SetPower, Stop, SetPosition
+from rovr_interfaces.srv import SetPower,SetPosition
 from rovr_interfaces.msg import LimitSwitches
+from std_srvs.srv import Trigger
 
 
 class DiggerNode(Node):
@@ -27,12 +28,12 @@ class DiggerNode(Node):
 
         # Define services (methods callable from the outside) here
         self.srv_toggle = self.create_service(SetPower, "digger/toggle", self.toggle_callback)
-        self.srv_stop = self.create_service(Stop, "digger/stop", self.stop_callback)
+        self.srv_stop = self.create_service(Trigger, "digger/stop", self.stop_callback)
         self.srv_setPower = self.create_service(SetPower, "digger/setPower", self.set_power_callback)
         self.srv_setPosition = self.create_service(SetPosition, "lift/setPosition", self.set_position_callback)
-        self.srv_lift_stop = self.create_service(Stop, "lift/stop", self.stop_lift_callback)
+        self.srv_lift_stop = self.create_service(Trigger, "lift/stop", self.stop_lift_callback)
         self.srv_lift_set_power = self.create_service(SetPower, "lift/setPower", self.lift_set_power_callback)
-        self.srv_zero_lift = self.create_service(Stop, "lift/zero", self.zero_lift_callback)
+        self.srv_zero_lift = self.create_service(Trigger, "lift/zero", self.zero_lift_callback)
 
         # Define publishers here
         self.publisher_goal_reached = self.create_publisher(Bool, "digger/goal_reached", 10)
