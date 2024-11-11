b4b0686f0b22b3a5e8df2a0713b3f63913b85ad8
alex berg <ber00221@umn.edu>
2024-11-11 10:57:56 -0600

    Always run build_image.sh when not trying to use cached image

--- scripts/enter_isaac_ros_container.sh
+++ scripts/enter_isaac_ros_container.sh
@@ -3,11 +3,13 @@ printf 'CONFIG_DOCKER_SEARCH_DIRS="$HOME/Lunabotics/src/isaac_ros/isaac_ros_comm
 image_key="ros2_humble.realsense.deepstream.user.zed.umn.gazebo"
 docker_arg="-v /usr/local/zed/resources:/usr/local/zed/resources -v $HOME/rosbags:/rosbags -v /usr/local/zed/settings:/usr/local/zed/settings"
 
-if docker images | grep -q "${image_key}"; then
+USE_CACHED_IMAGE=${1:-true}
+
+if $USE_CACHED_IMAGE && docker images | grep -q "${image_key}"; then
     echo "Image ${image_key} already exists"
 else
     echo "Building image ${image_key}"
-    bash ~/Lunabotics/scripts/build_image.sh ${1:-true}
+    bash ~/Lunabotics/scripts/build_image.sh $USE_CACHED_IMAGE
 fi
 
 bash ~/Lunabotics/src/isaac_ros/isaac_ros_common/scripts/run_dev.sh -d ~/Lunabotics -i "${image_key}" -a "${docker_arg}" -v -b
