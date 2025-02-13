0adb0c4368104098515d15d02784154983118995
CubeToPlay <hwvxyeej@gmail.com>
2025-02-13 01:07:46 +0000

    Fix calibrate_field_coordinate so that it now builds and supposidly registers the BT node

--- src/rovr_control/action/calibrate_field_coordinate.cpp
+++ src/rovr_control/action/calibrate_field_coordinate.cpp
@@ -3,23 +3,52 @@
 
 using CalibrateFieldCoordinate = rovr_interfaces::action::CalibrateFieldCoordinates;
 
-class CalibrateFieldCoordinateAction : public BT::RosActionNode<CalibrateFieldCoordinate> {
-    public:
-        CalibrateFieldCoordinateAction(
-            const std::string& name,
-            const BT::NodeConfig& conf,
-            const BT::RosNodeParams& params
-        )
-        : RosActionNode<CalibrateFieldCoordinate>(name, conf, params)
-        {}
+class CalibrateFieldCoordinateAction: public BT::RosActionNode<CalibrateFieldCoordinate> {
+public:
+    CalibrateFieldCoordinateAction(const std::string& name, const BT::NodeConfig& conf, const BT::RosNodeParams& params)
+    : BT::RosActionNode<CalibrateFieldCoordinate>(name, conf, params) {}
 
-        BT::NodeStatus onResultReceived(const WrappedResult& wr) override {
-            if (wr.result->success) {
-                return BT::NodeStatus::SUCCESS;
-            }
-            
-            return BT::NodeStatus::FAILURE;
-        }
+    // The specific ports of this Derived class
+    // should be merged with the ports of the base class,
+    // using RosActionNode::providedBasicPorts()
+    static BT::PortsList providedPorts()
+    {
+        return providedBasicPorts({BT::InputPort<unsigned>("order")});
+    }
+
+    // This is called when the TreeNode is ticked and it should
+    // send the request to the action server
+    bool setGoal(BT::RosActionNode<CalibrateFieldCoordinate>::Goal& goal) override 
+    {
+        // get "order" from the Input port
+        // getInput("order", goal.order);
+        // return true, if we were able to set the goal correctly.
+        return true;
+    }
+    
+    // Callback executed when the reply is received.
+    // Based on the reply you may decide to return SUCCESS or FAILURE.
+    BT::NodeStatus onResultReceived(const WrappedResult& wr) override
+    {
+        // std::stringstream ss;
+        // ss << "Result received: ";
+        // for (auto number : wr.result->sequence) {
+        // ss << number << " ";
+        // }
+        // RCLCPP_INFO(node_->get_logger(), ss.str().c_str());
+        return BT::NodeStatus::SUCCESS;
+    }
+
+    // Callback invoked when there was an error at the level
+    // of the communication between client and server.
+    // This will set the status of the TreeNode to either SUCCESS or FAILURE,
+    // based on the return value.
+    // If not overridden, it will return FAILURE by default.
+    virtual BT::NodeStatus onFailure(BT::ActionNodeErrorCode error) override
+    {
+        // RCLCPP_ERROR(node_->get_logger(), "Error: %d", error);
+        return BT::NodeStatus::FAILURE;
+    }
 };
 
 // Main method for the node
@@ -27,8 +56,14 @@ int main(int argc, char **argv) {
     // Initialize ROS 2
     rclcpp::init(argc, argv);
 
-    // Spin the node
-    // rclcpp::spin(std::make_shared<CalibrateFieldCoordinateAction>());
+    BT::BehaviorTreeFactory factory;
+
+    auto node = std::make_shared<rclcpp::Node>("calibrate_field_coordinate_action_client");
+    // provide the ROS node and the name of the action service
+    BT::RosNodeParams params; 
+    params.nh = node;
+    params.default_port_value = "calibrate_field_coordinate";
+    factory.registerNodeType<CalibrateFieldCoordinateAction>("CalibrateFieldCoordinate", params);
 
     // Free up any resources being used by the node
     rclcpp::shutdown();
