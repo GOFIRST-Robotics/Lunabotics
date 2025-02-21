82f21f17f8840ec83aefb6845f4b6a47143f8a68
akshatarinav <akshatarinav@gmail.com>
2025-02-20 19:53:57 -0600

    edited auto_offload.cpp

--- src/rovr_control/action/auto_offload.cpp
+++ src/rovr_control/action/auto_offload.cpp
@@ -1,42 +1,44 @@
 //Someone please double check this is correct
-#include "behaviortree_cpp/action_node.h"
-#include "/workspaces/Lunabotics/src/behaviortree_ros2/behaviortree_ros2/include/behaviortree_ros2/bt_action_node.hpp"
+#include "rovr_interfaces/action/auto_offload.hpp"
+#include "nav2_behavior_tree/bt_action_node.hpp"
+#include "nav2_behavior_tree/behavior_tree_engine.hpp"
 
+using AutoOffload = rovr_interfaces::action::AutoOffload;
 
-class AutoOffloadAction : public BT ::RosActionNode<AutoOffload>{
+class AutoOffloadAction : public nav2_behavior_tree::RosActionNode<AutoOffload>{
     public:
         AutoOffloadAction(
-            const std::string& name,
-            const BT::NodeConfig& conf,
-            const BT::RosNodeParams& params
-        )
-        : BT::RosActionNode<AutoOffload>(name, conf, params) {}
+            const std::string& xml_tag_name,
+            const std::string & action_name,
+            const BT::NodeConfiguration& conf)
+        : nav2_behavior_tree::BtActionNode<AutoOffload>(xml_tag_name, action_name, conf) {}
+        
+        //called when TreeNode is ticked, send request to action server
+        //bool setGoal(BT::RosActionNode<AutoOffload>::Goal& goal) override {
+           // return true;
+        //}
 
-        bool setGoal(BT::RosActionNode<AutoOffload>::Goal& goal) override {
-            return true;
-        }
-
-        BT::NodeStatus onResultReceived(const WrappedResult& wr) override{
-            return (wr.result->success) ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
-        }
+        //BT::NodeStatus onResultReceived(const WrappedResult& wr) override{
+           // return (wr.result->success) ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+        //}
 };
+
+BT_REGISTER_NODES(factory)
+{
+    BT::NodeBuilder builder =
+    [](const std::string & name, const BT::NodeConfiguration & config)
+    {
+        return std::make_unique<AutoOffload>(name, "auto_offload", config)
+    };
+
+    factory.registerBuilder<AutoOffload>("AutoOffload", builder);
+}
 
 //main method for the node
 int main(int argc, char **argv) {
     // Initialize ROS 2
     rclcpp::init(argc, argv);
 
-    BT::BehaviorTreeFactory factory;
-
-    auto node = std::make_shared<rclcpp::Node>("auto_offload_action_client");
-
-    // provide the ROs node and the name of the action service
-    BT::RosNodeParams params;
-    params.nh = node;
-    params.default_port_value = "auto_offload";
-    factory.registerNodeType<AutoOffloadAction>("AutoOffload", params);
-
-    // Free up any resources being used by the node
     rclcpp::shutdown();
     return 0;
 }
