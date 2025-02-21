89d8bc609390883f830a774db1233309e0a90818
KeerthanKrish <keerthankkrish@gmail.com>
2025-02-21 01:19:51 +0000

    added a go_to_dig_location action and interface

---
+++ src/rovr_control/action/go_to_dig_location.cpp
@@ -0,0 +1,34 @@
+#include "rovr_interfaces/action/go_to_dig_location.hpp"
+#include "nav2_behavior_tree/bt_action_node.hpp"
+#include "nav2_behavior_tree/behavior_tree_engine.hpp"
+
+using GoToDigLocation = rovr_interfaces::action::GoToDigLocation;
+
+class GoToDigLocationAction: public nav2_behavior_tree::BtActionNode<GoToDigLocation> {
+public:
+GoToDigLocation(
+        const std::string & xml_tag_name,
+        const std::string & action_name,
+        const BT::NodeConfiguration & conf)
+    : nav2_behavior_tree::BtActionNode<GoToDigLocation>(xml_tag_name, action_name, conf) {}
+};
+
+BT_REGISTER_NODES(factory)
+{
+  BT::NodeBuilder builder =
+    [](const std::string & name, const BT::NodeConfiguration & config)
+    {
+      return std::make_unique<GoToDigLocationAction>(name, "go_to_dig_location", config);
+    };
+
+  factory.registerBuilder<GoToDigLocationAction>("GoToDigLocation", builder);
+}
+
+// Main method for the node
+int main(int argc, char **argv) {
+    // Initialize ROS 2
+    rclcpp::init(argc, argv);
+    
+    rclcpp::shutdown();
+    return 0;
+}
