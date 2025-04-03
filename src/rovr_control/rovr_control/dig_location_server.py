5d86718f76ae2a9bee41c515a1380c52829e431f
alex berg <ber00221@umn.edu>
2025-04-02 19:57:30 -0500

    linter fix p2

--- src/rovr_control/rovr_control/dig_location_server.py
+++ src/rovr_control/rovr_control/dig_location_server.py
@@ -33,7 +33,7 @@ class DigLocationFinder(Node):
         self.absolute_max_dig_cost = self.declare_parameter("absolute_max_dig_cost", 200).value
         self.max_dig_cost = self.declare_parameter("max_dig_cost", 100).value
         self.all_dig_locations = self.declare_parameter(
-            "all_dig_locations", [.6, .37, .6, 1.1, .6, 1.83, 1.8, .37, 1.8, 1.1, 1.8, 1.83, 3.0, 
+            "all_dig_locations", [.6, .37, .6, 1.1, .6, 1.83, 1.8, .37, 1.8, 1.1, 1.8, 1.83, 3.0,
                                   .37, 3.0, 1.1, 3.0, 1.83, .6, 2.5, 1.8, 2.5, 3.0, 2.5]
         ).value  # If you default to an empty list things break (it thinks its a byte array)
 
