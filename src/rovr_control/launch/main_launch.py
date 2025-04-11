2df815795a1c3d13de08acc15a889df2ac70d4fd
CubeToPlay <hwvxyeej@gmail.com>
2025-04-11 00:39:21 +0000

    Fix names of nodes

--- src/rovr_control/launch/main_launch.py
+++ src/rovr_control/launch/main_launch.py
@@ -83,12 +83,12 @@ def generate_launch_description():
         name="auto_offload_server",
     )
 
-    # Add behavior tree action nodes
-    calibrate_field_coordinate = Node(
-        package="rovr_control",
-        executable="calibrate_field_coordinate",
-        name="calibrate_field_coordinate",
-    )
+    # # Add behavior tree action nodes
+    # calibrate_field_coordinate = Node(
+    #     package="rovr_control",
+    #     executable="calibrate_field_coordinate",
+    #     name="calibrate_field_coordinate",
+    # )
 
     ld.add_action(rovr_control)
     ld.add_action(motor_control)
@@ -101,6 +101,6 @@ def generate_launch_description():
     ld.add_action(calibrate_field_coordinate_server)
     ld.add_action(auto_dig_server)
     ld.add_action(auto_offload_server)
-    ld.add_action(calibrate_field_coordinate)
+    # ld.add_action(calibrate_field_coordinate)
 
     return ld
