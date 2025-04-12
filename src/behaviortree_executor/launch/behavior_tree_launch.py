54450e4f68d28d7e41a7a41a71279810678a6cdb
Jonathan Blixt <jmblixt3@gmail.com>
2025-04-12 11:45:41 -0500

    Initial reworked behavior trees

---
+++ src/behaviortree_executor/launch/behavior_tree_launch.py
@@ -0,0 +1,45 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    auto_dig_server = Node(
+        package="rovr_control",
+        executable="auto_dig_server",
+        name="auto_dig_server",
+    )
+    # auto_offload_server = Node(
+    #     package="rovr_control",
+    #     executable="auto_offload_server",
+    #     name="auto_offload_server",
+    # )
+
+    # # Add behavior tree action nodes
+    # calibrate_field_coordinate_server = Node(
+    #     package="rovr_control",
+    #     executable="calibrate_field_coordinate_server",
+    #     name="calibrate_field_coordinate_server",
+    # )
+
+    dig_location_server = Node(
+        package="rovr_control",
+        executable="dig_location_server",
+        name="dig_location_server",
+    )
+    
+    behaviortree_executor = Node(
+        package="behaviortree_executor",
+        executable="behaviortree_executor",
+        name="behaviortree_executor",
+        output="screen",
+        emulate_tty=True,
+    )
+    ld.add_action(auto_dig_server)
+    ld.add_action(auto_offload_server)
+    ld.add_action(calibrate_field_coordinate_server)
+    ld.add_action(dig_location_server)
+    ld.add_action(behaviortree_executor)
+
+    return ld
