269073ee57f86aa16312a6a3835c343b7be8a2e9
Jonathan Blixt <jmblixt3@gmail.com>
2025-04-13 17:41:09 -0500

    Kinda working

--- src/behaviortree_executor/src/action/auto_offload.hpp
+++ src/behaviortree_executor/src/action/auto_offload.hpp
@@ -1,4 +1,4 @@
-//Someone please double check this is correct
+// Someone please double check this is correct
 #include "rovr_interfaces/action/auto_offload.hpp"
 
 #include "behaviortree_cpp/tree_node.h"
@@ -8,36 +8,36 @@
 using AutoOffload = rovr_interfaces::action::AutoOffload;
 using namespace BT;
 
-class AutoOffloadAction : public RosActionNode<AutoOffload>{
-    public:
-        AutoOffloadAction(
-            const std::string& instance_name,
-            const NodeConfig& conf)
-        
-        : RosActionNode<AutoOffload>(
-            instance_name, 
-            conf, 
-            RosNodeParams(std::make_shared<rclcpp::Node>("auto_offload_node"))){}
+class AutoOffloadAction : public RosActionNode<AutoOffload>
+{
+public:
+    AutoOffloadAction(const std::string &name, const BT::NodeConfig &conf,
+                      const BT::RosNodeParams &params)
 
-        static PortsList providedPorts() {
-            return providedBasicPorts({
-                InputPort<double>("lift_dumping_position"),
-                InputPort<double>("digger_belt_power"),
-            });
-        }
-        
-        bool setGoal(Goal& goal) override 
-        {
-            // get inputs from the Input port
-            getInput("lift_dumping_position", goal.lift_dumping_position);
-            getInput("digger_belt_power", goal.digger_belt_power);
-            // return true, if we were able to set the goal correctly.
-            return true;
-        }
+        : RosActionNode<AutoOffload>(name, conf, params)
+    {
+    }
 
-        //TODO: finish this
-        NodeStatus onResultReceived(__attribute__ ((unused)) const WrappedResult& result) override {
-            return NodeStatus::SUCCESS;
-        }   
+    static PortsList providedPorts()
+    {
+        return providedBasicPorts({
+            InputPort<double>("lift_dumping_position"),
+            InputPort<double>("digger_belt_power"),
+        });
+    }
 
+    bool setGoal(Goal &goal) override
+    {
+        // get inputs from the Input port
+        getInput("lift_dumping_position", goal.lift_dumping_position);
+        getInput("digger_belt_power", goal.digger_belt_power);
+        // return true, if we were able to set the goal correctly.
+        return true;
+    }
+
+    // TODO: finish this
+    NodeStatus onResultReceived(__attribute__((unused)) const WrappedResult &result) override
+    {
+        return NodeStatus::SUCCESS;
+    }
 };
