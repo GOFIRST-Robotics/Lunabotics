d3b755b0731cf433b4ffbe2049bc986de5a3dda4
cparece1 <53668869+cparece1@users.noreply.github.com>
2025-04-16 18:51:28 -0500

    Bad Solution
    This resets the device in usb port 1's connection after the joy node is running- but no sure bet that USB port 1 has the controller

--- scripts/laptop.sh
+++ scripts/laptop.sh
@@ -2,3 +2,7 @@
 
 source install/setup.bash && \
 ros2 launch gstreamer laptop_launch.py
+
+sudo sh -c "echo 0 > /sys/bus/usb/devices/1-1/authorized"
+sleep 0.2
+sudo sh -c "echo 1 > /sys/bus/usb/devices/1-1/authorized"
