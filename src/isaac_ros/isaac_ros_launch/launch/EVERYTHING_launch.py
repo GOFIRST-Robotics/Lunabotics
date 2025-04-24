c80fee0902af77a1143d884cd87822e67e9fc5c4
umnrobotics <robotics@umn.edu>
2025-04-23 19:26:44 -0500

    nvblox+nav2 works well now!

--- src/isaac_ros/isaac_ros_launch/launch/EVERYTHING_launch.py
+++ src/isaac_ros/isaac_ros_launch/launch/EVERYTHING_launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
             "setup_for_gazebo": "False",
             "use_nvblox": "True",
             "run_rviz_robot": "False",  # We don't need to run RViz during matches
-            "zed_multicam": "False",  # Use multiple ZED cameras
+            "zed_multicam": "True",  # Use multiple ZED cameras
             "record_svo": "True",  # Record match data to an SVO file
         }.items(),
     )
