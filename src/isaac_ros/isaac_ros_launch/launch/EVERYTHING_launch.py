4477c7cfc7e2368a81ad860267434d5a7f8a054a
alex berg <ber00221@umn.edu>
2025-05-07 21:24:40 -0500

    added to everything launch

--- src/isaac_ros/isaac_ros_launch/launch/EVERYTHING_launch.py
+++ src/isaac_ros/isaac_ros_launch/launch/EVERYTHING_launch.py
@@ -29,18 +29,14 @@ def generate_launch_description():
         }.items(),
     )
 
-    gstreamer_server = Node(
-        package="gstreamer",
-        executable="server_node",
-        name="gstreamer_server_node",
-        output="screen",
-        emulate_tty=True,
+    camera_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(PathJoinSubstitution([FindPackageShare("rovr_control"), "camera_launch.py"]))
     )
 
     # Add all of the actions to the launch description
     ld.add_action(main_launch)
-    ld.add_action(gstreamer_server)
     ld.add_action(isaac_launch)
+    ld.add_action(camera_launch)
 
     # Return the launch description
     return ld
