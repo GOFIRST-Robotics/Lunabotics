25107ff198c27c055a645dc7011a9d1af0d1a0e1
hdjwis <92123937+hdjwis@users.noreply.github.com>
2026-02-05 18:39:52 -0600

    Updated dumper (auger) (#428)
    * "Updated dumper conditions for new design based limit switches"
    
    * Created Sub/Pub for Limit Switches  and check if both actuators were retracted for dumping"
    
    * Added Conditionals to Extend Dumper and changed constant values
    
    * updated struct.unpack
    
    * updated limit switch msg type
    
    * Change limit switch types from int to bool
    
    * Reset pulled_state in drop_dumper
    
    * linter fixes
    
    * linter fixes
    
    * updated auto offload
    
    * Changed to position control in pull and drop dumper function. Removed top kill switch and replaced it with degree conditional in while loop instead
    
    * commented auger checks from dumper node, for now
    
    * Changed names in autoOffLoad to match dumper_node server nodes
    
    * Final updates for dumper
    
    * renamed dumper services to dump and store
    
    * inlcuded Bool std_msg
    
    ---------
    
    Co-authored-by: Team Laptop 1 <robotics@umn.edu>
    Co-authored-by: zheng271 <brendonzheng57@gmail.com>
    Co-authored-by: alex berg <ber00221@umn.edu>

--- src/rovr_control/rovr_control/read_serial.py
+++ src/rovr_control/rovr_control/read_serial.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from rovr_interfaces.msg import Potentiometers
 from std_srvs.srv import SetBool, Trigger
+from std_msgs.msg import Bool
 
 import serial
 import struct
@@ -13,6 +14,7 @@ class read_serial(Node):
         super().__init__("read_serial")
 
         self.potentiometerPub = self.create_publisher(Potentiometers, "potentiometers", 10)
+        self.LimitSwitchPub = self.create_publisher(Bool, "DumperLimitSwitch", 10)
 
         # Services to control the relay-driven agitator motor
         self.srv_bigonoff = self.create_service(SetBool, "big_agitator_on_off", self.big_on_off_callback)
@@ -31,7 +33,9 @@ class read_serial(Node):
             return
 
         self.timer = self.create_timer(0.1, self.timer_callback)
+
         self.lastMsg = Potentiometers()
+
         self.bigAgitatorOn = False
         self.smallAgitatorOn = False
 
@@ -41,13 +45,18 @@ class read_serial(Node):
             self.destroy_node()
             return
         data = self.arduino.read(4)  # Pause until 4 bytes are read
-        decoded = struct.unpack("hh", data)  # Use h for integers and ? for booleans
+        decoded = struct.unpack("hh?", data)  # Use h for integers and ? for booleans
 
         msg = Potentiometers()
         msg.left_motor_pot = decoded[0]
         msg.right_motor_pot = decoded[1]
         self.potentiometerPub.publish(msg)
         self.lastMsg = msg
+
+        bool_msg = Bool()
+        bool_msg.data = decoded[2]
+        self.LimitSwitchPub.publish(bool_msg)
+        self.lastMsg = bool_msg
 
     def big_on_off_callback(self, request, response):
         # request.data == True  → ON, False → OFF
