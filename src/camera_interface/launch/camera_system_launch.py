da0c10dbf0daca603fd5d89497fcf4e8a852b2bc
umnrobotics <robotics@umn.edu>
2026-03-03 19:45:33 -0600

    Experimenting with different USB ids

--- src/camera_interface/launch/camera_system_launch.py
+++ src/camera_interface/launch/camera_system_launch.py
@@ -3,14 +3,15 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # NOTE: Instead of an index, you can use the stable path found in /dev/v4l/by-id/ or /dev/v4l/by-path/
+    # To get the list of ids, we do : ls -l /dev/v4l/by-id/
     # (e.g.) -> /dev/v4l/by-id/usb-Logitech_Webcam_C920_ABC123-video-index0
     camera_configs = [
-        {"name": "right", "topic": "/webcam/right", "id": "/dev/video6"},
-        {"name": "left",  "topic": "/webcam/left",  "id": "/dev/video8"},
-        {"name": "back",  "topic": "/webcam/back",  "id": "/dev/video0"},
-        {"name": "digger", "topic": "/webcam/digger", "id": "/dev/video4"},
-        {"name": "dumper",  "topic": "/webcam/dumper",  "id": "/dev/video5"},
-        {"name": "front",  "topic": "/webcam/front",  "id": "/dev/video0"},
+        {"name": "right", "topic": "/webcam/right", "id": "/dev/v4l/by-id/usb-Arducam_Technology_Co.__Ltd._Arducam_OV9281_USB_Camera_UC762-video-index0"},
+        {"name": "left",  "topic": "/webcam/left",  "id": "/dev/v4l/by-id/usb-Arducam_Technology_Co.__Ltd._Arducam_OV9782_USB_Camera_UC852-video-index0"},
+        {"name": "back",  "topic": "/webcam/back",  "id": "/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._USB2.0_FHD_UVC_WebCam-video-index0"},
+        {"name": "digger", "topic": "/webcam/digger", "id": "/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._USB2.0_FHD_UVC_WebCam-video-index1"},
+        {"name": "dumper",  "topic": "/webcam/dumper",  "id": "/dev/video4"},
+        {"name": "front",  "topic": "/webcam/front",  "id": "/dev/video5"},
     ]
 
     nodes = [
