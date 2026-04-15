e2d19c2ccea969171b872b9b670a2cac82efe7e0
Adam Yaj <hwvxyeej@gmail.com>
2026-04-14 19:47:55 -0500

    Add basic launch

---
+++ src/behavior_control/launch/behavior_control_launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='behavior_control',
+            executable='behavior_control_tree',
+        )
+    ])
